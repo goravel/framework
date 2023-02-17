d768765731b3a4b22446ea3f581e0ef7d3506529
Wenbo han <wenbo.han@compass.com>
2023-02-17 22:47:05 +0800

    Optimize tls middleware

--- http/middleware/tls.go
+++ http/middleware/tls.go
@@ -6,13 +6,24 @@ import (
 	"github.com/unrolled/secure"
 
 	"github.com/goravel/framework/contracts/http"
+	"github.com/goravel/framework/facades"
 )
 
-func Tls(attr string) http.Middleware {
+func Tls(host ...string) http.Middleware {
 	return func(ctx http.Context) {
+		if len(host) == 0 {
+			defaultHost := facades.Config.GetString("route.tls.host")
+			if defaultHost == "" {
+				ctx.Request().Next()
+
+				return
+			}
+			host = append(host, defaultHost)
+		}
+
 		secureMiddleware := secure.New(secure.Options{
 			SSLRedirect: true,
-			SSLHost:     attr,
+			SSLHost:     host[0],
 		})
 
 		if err := secureMiddleware.Process(ctx.Response().Writer(), ctx.Request().Origin()); err != nil {
