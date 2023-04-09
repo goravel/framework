0dc93f2bf4efba1266d8147ef14f13db6b443a01
Wenbo han <wenbo.han@compass.com>
2023-04-09 22:54:07 +0800

    Add Fallback method to facades.Route

--- contracts/route/route.go
+++ contracts/route/route.go
@@ -3,7 +3,7 @@ package route
 import (
 	"net/http"
 
-	httpcontract "github.com/goravel/framework/contracts/http"
+	contractshttp "github.com/goravel/framework/contracts/http"
 )
 
 type GroupFunc func(routes Route)
@@ -11,26 +11,27 @@ type GroupFunc func(routes Route)
 //go:generate mockery --name=Engine
 type Engine interface {
 	Route
+	Fallback(handler contractshttp.HandlerFunc)
+	GlobalMiddleware(middlewares ...contractshttp.Middleware)
 	Run(host ...string) error
 	RunTLS(host ...string) error
 	RunTLSWithCert(host, certFile, keyFile string) error
 	ServeHTTP(writer http.ResponseWriter, request *http.Request)
-	GlobalMiddleware(middlewares ...httpcontract.Middleware)
 }
 
 //go:generate mockery --name=Route
 type Route interface {
 	Group(handler GroupFunc)
 	Prefix(addr string) Route
-	Middleware(middlewares ...httpcontract.Middleware) Route
+	Middleware(middlewares ...contractshttp.Middleware) Route
 
-	Any(relativePath string, handler httpcontract.HandlerFunc)
-	Get(relativePath string, handler httpcontract.HandlerFunc)
-	Post(relativePath string, handler httpcontract.HandlerFunc)
-	Delete(relativePath string, handler httpcontract.HandlerFunc)
-	Patch(relativePath string, handler httpcontract.HandlerFunc)
-	Put(relativePath string, handler httpcontract.HandlerFunc)
-	Options(relativePath string, handler httpcontract.HandlerFunc)
+	Any(relativePath string, handler contractshttp.HandlerFunc)
+	Get(relativePath string, handler contractshttp.HandlerFunc)
+	Post(relativePath string, handler contractshttp.HandlerFunc)
+	Delete(relativePath string, handler contractshttp.HandlerFunc)
+	Patch(relativePath string, handler contractshttp.HandlerFunc)
+	Put(relativePath string, handler contractshttp.HandlerFunc)
+	Options(relativePath string, handler contractshttp.HandlerFunc)
 
 	Static(relativePath, root string)
 	StaticFile(relativePath, filepath string)
