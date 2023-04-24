a594cbd5979512501680b2d25bbd56894ddec3c7
Wenbo Han <hwbrzzl@gmail.com>
2023-04-24 16:02:21 +0800

    Add net/http Method map to contracts/http (#102)

---
+++ contracts/http/method.go
@@ -0,0 +1,17 @@
+package http
+
+import (
+	"net/http"
+)
+
+const (
+	MethodGet     = http.MethodGet
+	MethodHead    = http.MethodHead
+	MethodPost    = http.MethodPost
+	MethodPut     = http.MethodPut
+	MethodPatch   = http.MethodPatch
+	MethodDelete  = http.MethodDelete
+	MethodConnect = http.MethodConnect
+	MethodOptions = http.MethodOptions
+	MethodTrace   = http.MethodTrace
+)
