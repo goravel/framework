75af845d31d6aae2e58c224e7d1861c06b6f3f4f
Wenbo han <wenbo.han@compass.com>
2023-05-19 22:48:20 +0800

    Adaptation Route module

--- route/gin.go
+++ route/gin.go
@@ -2,35 +2,40 @@ package route
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 	"github.com/gookit/color"
 
+	"github.com/goravel/framework/contracts/config"
 	httpcontract "github.com/goravel/framework/contracts/http"
 	"github.com/goravel/framework/contracts/route"
-	"github.com/goravel/framework/facades"
 	goravelhttp "github.com/goravel/framework/http"
 )
 
 type Gin struct {
 	route.Route
+	config   config.Config
 	instance *gin.Engine
 }
 
-func NewGin() *Gin {
+func NewGin(config config.Config) *Gin {
 	gin.SetMode(gin.ReleaseMode)
 	engine := gin.New()
-	if debugLog := getDebugLog(); debugLog != nil {
+	if debugLog := getDebugLog(config); debugLog != nil {
 		engine.Use(debugLog)
 	}
 
-	return &Gin{instance: engine, Route: NewGinGroup(
-		engine.Group("/"),
-		"",
-		[]httpcontract.Middleware{},
-		[]httpcontract.Middleware{goravelhttp.GinResponseMiddleware()},
-	)}
+	return &Gin{
+		Route: NewGinGroup(engine.Group("/"),
+			"",
+			[]httpcontract.Middleware{},
+			[]httpcontract.Middleware{goravelhttp.GinResponseMiddleware()},
+		),
+		config:   config,
+		instance: engine,
+	}
 }
 
 func (r *Gin) Fallback(handler httpcontract.HandlerFunc) {
@@ -51,12 +56,12 @@ func (r *Gin) GlobalMiddleware(middlewares ...httpcontract.Middleware) {
 
 func (r *Gin) Run(host ...string) error {
 	if len(host) == 0 {
-		defaultHost := facades.Config.GetString("http.host")
+		defaultHost := r.config.GetString("http.host")
 		if defaultHost == "" {
 			return errors.New("host can't be empty")
 		}
 
-		defaultPort := facades.Config.GetString("http.port")
+		defaultPort := r.config.GetString("http.port")
 		if defaultPort == "" {
 			return errors.New("port can't be empty")
 		}
@@ -64,7 +69,7 @@ func (r *Gin) Run(host ...string) error {
 		host = append(host, completeHost)
 	}
 
-	outputRoutes(r.instance.Routes())
+	r.outputRoutes()
 	color.Greenln("[HTTP] Listening and serving HTTP on " + host[0])
 
 	return r.instance.Run([]string{host[0]}...)
@@ -72,12 +77,12 @@ func (r *Gin) Run(host ...string) error {
 
 func (r *Gin) RunTLS(host ...string) error {
 	if len(host) == 0 {
-		defaultHost := facades.Config.GetString("http.tls.host")
+		defaultHost := r.config.GetString("http.tls.host")
 		if defaultHost == "" {
 			return errors.New("host can't be empty")
 		}
 
-		defaultPort := facades.Config.GetString("http.tls.port")
+		defaultPort := r.config.GetString("http.tls.port")
 		if defaultPort == "" {
 			return errors.New("port can't be empty")
 		}
@@ -85,8 +90,8 @@ func (r *Gin) RunTLS(host ...string) error {
 		host = append(host, completeHost)
 	}
 
-	certFile := facades.Config.GetString("http.tls.ssl.cert")
-	keyFile := facades.Config.GetString("http.tls.ssl.key")
+	certFile := r.config.GetString("http.tls.ssl.cert")
+	keyFile := r.config.GetString("http.tls.ssl.key")
 
 	return r.RunTLSWithCert(host[0], certFile, keyFile)
 }
@@ -99,7 +104,7 @@ func (r *Gin) RunTLSWithCert(host, certFile, keyFile string) error {
 		return errors.New("certificate can't be empty")
 	}
 
-	outputRoutes(r.instance.Routes())
+	r.outputRoutes()
 	color.Greenln("[HTTPS] Listening and serving HTTPS on " + host)
 
 	return r.instance.RunTLS(host, certFile, keyFile)
@@ -107,4 +112,12 @@ func (r *Gin) RunTLSWithCert(host, certFile, keyFile string) error {
 
 func (r *Gin) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	r.instance.ServeHTTP(writer, request)
+}
+
+func (r *Gin) outputRoutes() {
+	if r.config.GetBool("app.debug") && !runningInConsole() {
+		for _, item := range r.instance.Routes() {
+			fmt.Printf("%-10s %s\n", item.Method, colonToBracket(item.Path))
+		}
+	}
 }
