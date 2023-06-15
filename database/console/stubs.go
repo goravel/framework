e742ecee57cae400984a07b9e1c6ab0c45949499
kkumar-gcc <krishkumar9352@gmail.com>
2023-06-15 13:02:18 +0530

    add use cases description in seeder stub

--- database/console/stubs.go
+++ database/console/stubs.go
@@ -102,9 +102,31 @@ type %s struct {
 }
 	
 func (s *%s) Run(ctx console.Context) error {
-	// Perform your seeding operations here
-	// Access the container and command if needed: s.Container, s.Command
+	// Run executes the seeder logic.
+	// To use the %s, register it in a service provider by calling facades.Seeder().Register and passing an instance of the %s as a pointer.
+	// Example:
+	//     facades.Seeder().Register([]database.Seeder{
+	//         ...
+	//         &seeders.%s{},
+	//         ...
+	//     })
+	//
+	// After registering, run the seeder by invoking the seed command in the console.
+	// Example:
+    //     go run . artisan db:seed --class=%s
+	//
+	// The Run method performs the actual seeding operations.
+	// To register other seeders to run, use the CallOnce method and provide the seeder instances.
+	// Example:
+	//     // Register multiple seeders
+	//     s.CallOnce([]interface{}{&seeders.&OtherSeeder{},&seeders.&OtherSeeder2{}}, true, nil)
+	//
+	//     // Register a single seeder
+	//     s.CallOnce(&seeders.&OtherSeeder{}, true, nil)
+	//
+	// Make sure to adjust the import statements and package paths based on your project structure.
+
 	return nil
 }
-`, name, name)
+`, name, name, name, name, name, name)
 }
