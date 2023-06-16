9596ec146db8fe552417d79ac34d6811ca8837c3
kkumar-gcc <krishkumar9352@gmail.com>
2023-06-16 21:05:45 +0530

    remove unnecessary comments from seeder stub

--- database/console/stubs.go
+++ database/console/stubs.go
@@ -72,7 +72,6 @@ func (u *DummyObserver) ForceDeleted(event orm.Event) error {
 `
 }
 
-
 func (r Stubs) Seeder() string {
 	return `package DummyPackage
 
@@ -86,26 +85,7 @@ type DummySeeder struct {
 	
 func (s *DummySeeder) Run() error {
 	// Run executes the seeder logic.
-	// To use the DummySeeder, register it in a service provider by calling facades.Seeder().Register and passing an instance of the DummySeeder as a pointer.
-	// Example:
-	//     facades.Seeder().Register([]seeder.Seeder{
-	//         ...
-	//         &seeders.DummySeeder{},
-	//         ...
-	//     })
-	//
-	// After registering, run the seeder by invoking the seed command in the console.
-	// Example:
-    //     go run . artisan db:seed --seeder DummySeeder
-	//
-	// The Run method performs the actual seeding operations.
-	// To register other seeders to run, use the CallOnce method and provide the seeder instances.
-	// Example:
-	//     // Register multiple seeders
-	//     s.CallOnce([]seeder.Seeder{&seeders.&OtherSeeder{},&seeders.&OtherSeeder2{}})
-	//
-	// Make sure to adjust the import statements and package paths based on your project structure.
-
+	
 	return nil
 }
 `
