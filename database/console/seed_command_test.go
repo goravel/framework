fe89ac423c2754d88aaad0871d64320289b73795
kkumar-gcc <krishkumar9352@gmail.com>
2023-06-21 17:07:08 +0530

    add test suite for db:seed command

--- database/console/seed_command_test.go
+++ database/console/seed_command_test.go
@@ -5,69 +5,46 @@ import (
 
 	configmocks "github.com/goravel/framework/contracts/config/mocks"
 	consolemocks "github.com/goravel/framework/contracts/console/mocks"
-
-	// appmocks "github.com/goravel/framework/contracts/foundation/mocks"
-
+	seedermocks "github.com/goravel/framework/contracts/database/seeder/mocks"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/suite"
 )
 
-func TestSeedCommand_Handle(t *testing.T) {
-	// // Create mock config
-	// mockConfig := &configmocks.Config{}
-	// mockConfig.On("Env", "APP_ENV").Return("development")
-
-	// // Create mock seeder facade
-
-	// mockApp := &appmocks.Application{}
-	// mockSeeder := mockApp.MakeSeeder()
-	// mockSeeder.On("GetSeeders").Return([]seeder.Seeder{
-	// 	&mocks.Seeder{},
-	// }, nil)
-
-	// // Create command instance
-	// command := NewSeedCommand(mockConfig, mockSeeder)
-
-	// // Create mock context
-	// mockContext := &consolemocks.Context{}
-	// mockContext.On("OptionSlice", "seeder").Return([]string{})
-
-	// // Test Handle method with no seeders specified
-	// err := command.Handle(mockContext)
-	// assert.NoError(t, err)
-	// mockSeeder.AssertCalled(t, "GetSeeders")
-
-	// // Create mock context with seeders specified
-	// mockContext.On("OptionSlice", "seeder").Return([]string{"Seeder1", "Seeder2"})
-	// mockSeeder.On("GetSeeder", "seeders.Seeder1").Return(&mocks.Seeder{}, nil)
-	// mockSeeder.On("GetSeeder", "seeders.Seeder2").Return(&mocks.Seeder{}, nil)
-
-	// // Test Handle method with seeders specified
-	// err = command.Handle(mockContext)
-	// assert.NoError(t, err)
-	// mockSeeder.AssertCalled(t, "GetSeeder", "seeders.Seeder1")
-	// mockSeeder.AssertCalled(t, "GetSeeder", "seeders.Seeder2")
+type SeedCommandTestSuite struct {
+	suite.Suite
+	mockConfig  *configmocks.Config
+	mockFacade  *seedermocks.Facade
+	mockContext *consolemocks.Context
+	seedCommand *SeedCommand
 }
 
-func TestSeedCommand_ConfirmToProceed(t *testing.T) {
-	// Create mock config
-	mockConfig := &configmocks.Config{}
-	mockConfig.On("Env", "APP_ENV").Return("production")
+func TestSeedCommandTestSuite(t *testing.T) {
+	suite.Run(t, new(SeedCommandTestSuite))
+}
 
-	// Create command instance
-	command := NewSeedCommand(mockConfig, nil)
+func (s *SeedCommandTestSuite) SetupTest() {
+	s.mockConfig = &configmocks.Config{}
+	s.mockFacade = &seedermocks.Facade{}
+	s.mockContext = &consolemocks.Context{}
+	s.seedCommand = NewSeedCommand(s.mockConfig, s.mockFacade)
+}
 
-	// Create mock context with force option
-	mockContext := &consolemocks.Context{}
-	mockContext.On("OptionBool", "force").Return(true).Once()
+func (s *SeedCommandTestSuite) TestHandle() {
+	// write test for handle function here
+}
 
-	// Test ConfirmToProceed method with force option
-	err := command.ConfirmToProceed(mockContext)
-	assert.NoError(t, err)
+func (s *SeedCommandTestSuite) TestConfirmToProceed() {
+	s.mockConfig.On("Env", "APP_ENV").Return("production").Once()
+	s.mockContext.On("OptionBool", "force").Return(true).Once()
+	err := s.seedCommand.ConfirmToProceed(true)
+	assert.NoError(s.T(), err)
 
-	// Create mock context without force option
-	mockContext.On("OptionBool", "force").Return(false).Once()
+	s.mockContext.On("OptionBool", "force").Return(false).Once()
 
-	// Test ConfirmToProceed method without force option
-	err = command.ConfirmToProceed(mockContext)
-	assert.EqualError(t, err, "application in production use --force to run this command")
+	err = s.seedCommand.ConfirmToProceed(false)
+	assert.EqualError(s.T(), err, "application in production use --force to run this command")
+}
+
+func (s *SeedCommandTestSuite) TestGetSeeders() {
+	// write logic for GetSeeders function here
 }
