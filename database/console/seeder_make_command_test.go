e3328262aefb1d3ff4d5401c3c7289028f4bf15f
kkumar-gcc <krishkumar9352@gmail.com>
2023-06-22 21:28:34 +0530

    refactor test for make:seeder command

--- database/console/seeder_make_command_test.go
+++ database/console/seeder_make_command_test.go
@@ -19,10 +19,14 @@ func TestSeederMakeCommand(t *testing.T) {
 	mockContext.On("Argument", 0).Return("UserSeeder").Once()
 	assert.Nil(t, seederMakeCommand.Handle(mockContext))
 	assert.True(t, file.Exists("database/seeders/user_seeder.go"))
-	assert.True(t, file.Remove("database"))
+	assert.True(t, file.Contain("database/seeders/user_seeder.go", "package seeders"))
+	assert.True(t, file.Contain("database/seeders/user_seeder.go", "type UserSeeder struct"))
+	assert.Nil(t, file.Remove("database"))
 
 	mockContext.On("Argument", 0).Return("subdir/DemoSeeder").Once()
 	assert.Nil(t, seederMakeCommand.Handle(mockContext))
 	assert.True(t, file.Exists("database/seeders/subdir/demo_seeder.go"))
-	assert.True(t, file.Remove("database"))
+	assert.True(t, file.Contain("database/seeders/subdir/demo_seeder.go", "package subdir"))
+	assert.True(t, file.Contain("database/seeders/subdir/demo_seeder.go", "type DemoSeeder struct"))
+	assert.Nil(t, file.Remove("database"))
 }
