58d443fdc45cb2df25047b3f3958ad802b49801e
Flc゛ <four_leaf_clover@foxmail.com>
2023-07-04 13:05:52 +0800

    tests: Optimize test

--- support/debug/dump_test.go
+++ support/debug/dump_test.go
@@ -1,20 +1,46 @@
 package debug
 
 import (
-	"fmt"
-	"github.com/stretchr/testify/assert"
+	"bytes"
 	"os"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
+func redirectStdout(fn func()) ([]byte, error) {
+	f, err := os.CreateTemp("", "stdout")
+	if err != nil {
+		return nil, err
+	}
+	defer os.Remove(f.Name())
+	defer f.Close()
+
+	orig := os.Stdout
+	os.Stdout = f
+	fn()
+	os.Stdout = orig
+
+	return os.ReadFile(f.Name())
+}
+
 func TestDump(t *testing.T) {
-	Dump("foo")
+	buf, err := redirectStdout(func() {
+		Dump("foo")
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, `(string) (len=3) "foo"
+`, string(buf))
 }
 
 func TestFDump(t *testing.T) {
-	FDump(os.Stdout, "foo")
+	var buf bytes.Buffer
+	w := &buf
 
-	fmt.Sprintf("%s", SDump("foo"))
+	FDump(w, "foo")
+
+	assert.Equal(t, `(string) (len=3) "foo"
+`, buf.String())
 }
 
 func TestSDump(t *testing.T) {
