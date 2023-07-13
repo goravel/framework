4bdea48047bfa941e0bfccbd07a6925fe118c01b
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2023-07-13 17:21:04 -0300

    add test for resource

--- http/console/controller_make_command_test.go
+++ http/console/controller_make_command_test.go
@@ -29,3 +29,21 @@ func TestControllerMakeCommand(t *testing.T) {
 	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "type AuthController struct"))
 	assert.Nil(t, file.Remove("app"))
 }
+
+func TestResourceControllerMakeCommand(t *testing.T) {
+	controllerMakeCommand := &ControllerMakeCommand{}
+	mockContext := &consolemocks.Context{}
+	mockContext.On("Argument", 0).Return("User/AuthController").Once()
+	mockContext.On("OptionBool", "resource").Return(true).Once()
+	err := controllerMakeCommand.Handle(mockContext)
+	assert.Nil(t, err)
+	assert.True(t, file.Exists("app/http/controllers/User/auth_controller.go"))
+	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "package User"))
+	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "type AuthController struct"))
+	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "func (r *AuthController) Index(ctx http.Context) {"))
+	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "func (r *AuthController) Show(ctx http.Context) {"))
+	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "func (r *AuthController) Store(ctx http.Context) {"))
+	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "func (r *AuthController) Update(ctx http.Context) {"))
+	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "func (r *AuthController) Destroy(ctx http.Context) {"))
+	assert.Nil(t, file.Remove("app"))
+}
