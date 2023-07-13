296d136c112678e822080fe96fff2360811480a9
Mauri de Souza Meneguzzo <mauri870@gmail.com>
2023-07-13 17:23:01 -0300

    mock OptionBool on base test

--- http/console/controller_make_command_test.go
+++ http/console/controller_make_command_test.go
@@ -17,16 +17,19 @@ func TestControllerMakeCommand(t *testing.T) {
 	assert.EqualError(t, err, "Not enough arguments (missing: name) ")
 
 	mockContext.On("Argument", 0).Return("UsersController").Once()
+	mockContext.On("OptionBool", "resource").Return(false).Once()
 	err = controllerMakeCommand.Handle(mockContext)
 	assert.Nil(t, err)
 	assert.True(t, file.Exists("app/http/controllers/users_controller.go"))
 
 	mockContext.On("Argument", 0).Return("User/AuthController").Once()
+	mockContext.On("OptionBool", "resource").Return(false).Once()
 	err = controllerMakeCommand.Handle(mockContext)
 	assert.Nil(t, err)
 	assert.True(t, file.Exists("app/http/controllers/User/auth_controller.go"))
 	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "package User"))
 	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "type AuthController struct"))
+	assert.True(t, file.Contain("app/http/controllers/User/auth_controller.go", "func (r *AuthController) Index(ctx http.Context) {"))
 	assert.Nil(t, file.Remove("app"))
 }
 
