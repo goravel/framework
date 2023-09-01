69f2629b648f9aab945f8f85595581c668c31747
kkumar-gcc <krishkumar9352@gmail.com>
2023-09-01 16:17:35 +0530

    add TestRequest and Response

--- log/logrus_writer_test.go
+++ log/logrus_writer_test.go
@@ -1,8 +1,10 @@
 package log
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	nethttp "net/http"
 	"os"
 	"os/exec"
 	"reflect"
@@ -12,6 +14,9 @@ import (
 	"github.com/stretchr/testify/suite"
 
 	configmock "github.com/goravel/framework/contracts/config/mocks"
+	"github.com/goravel/framework/contracts/filesystem"
+	contractshttp "github.com/goravel/framework/contracts/http"
+	"github.com/goravel/framework/contracts/validation"
 	"github.com/goravel/framework/support/carbon"
 	"github.com/goravel/framework/support/file"
 )
@@ -266,11 +271,24 @@ func (s *LogrusTestSuite) TestLogrus() {
 				mockDriverConfig(mockConfig)
 
 				log = NewLogrusApplication(mockConfig)
-				log.Request(nil).Info("Goravel")
+				log.Request(&TestRequest{}).Info("Goravel")
 			},
 			assert: func() {
-				assert.True(s.T(), file.Contain(singleLog, "test.info: Goravel"))
-				assert.True(s.T(), file.Contain(dailyLog, "test.info: Goravel"))
+				expectedParts := []string{
+					`test.info: Goravel`,
+					`request: {`,
+					`"method":"GET`,
+					`"uri":"http://localhost:3000/"`,
+					`"header":{"Sec-Fetch-User":["?1"],"Host":["localhost:3000"]}`,
+					`"body":{`,
+					`"key1":"value1"`,
+					`"key2":"value2"`,
+				}
+
+				for _, part := range expectedParts {
+					assert.True(s.T(), file.Contain(singleLog, part))
+					assert.True(s.T(), file.Contain(dailyLog, part))
+				}
 			},
 		},
 		{
@@ -279,11 +297,22 @@ func (s *LogrusTestSuite) TestLogrus() {
 				mockDriverConfig(mockConfig)
 
 				log = NewLogrusApplication(mockConfig)
-				log.Response(nil).Info("Goravel")
+				log.Response(&TestResponse{}).Info("Goravel")
 			},
 			assert: func() {
-				assert.True(s.T(), file.Contain(singleLog, "test.info: Goravel"))
-				assert.True(s.T(), file.Contain(dailyLog, "test.info: Goravel"))
+				expectedParts := []string{
+					`test.info: Goravel`,
+					`response: {`,
+					`"status":200`,
+					`"header":{"Content-Type":["text/plain; charset=utf-8"]}`,
+					`"body":{}`,
+					`"size":4`,
+				}
+
+				for _, part := range expectedParts {
+					assert.True(s.T(), file.Contain(singleLog, part))
+					assert.True(s.T(), file.Contain(dailyLog, part))
+				}
 			},
 		},
 		{
@@ -400,4 +429,260 @@ func mockDriverConfig(mockConfig *configmock.Config) {
 	mockConfig.On("GetString", "logging.channels.single.level").Return("debug").Once()
 	mockConfig.On("GetString", "app.timezone").Return("UTC")
 	mockConfig.On("GetString", "app.env").Return("test")
+}
+
+type TestRequest struct{}
+
+func (r *TestRequest) Header(key string, defaultValue ...string) string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) Headers() nethttp.Header {
+	return nethttp.Header{
+		"Sec-Fetch-User": []string{"?1"},
+		"Host":           []string{"localhost:3000"},
+	}
+}
+
+func (r *TestRequest) Method() string {
+	return "GET"
+}
+
+func (r *TestRequest) Path() string {
+	return "/test"
+}
+
+func (r *TestRequest) Url() string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) FullUrl() string {
+	return "http://localhost:3000/"
+}
+
+func (r *TestRequest) Ip() string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) Host() string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) All() map[string]any {
+	return map[string]interface{}{
+		"key1": "value1",
+		"key2": "value2",
+	}
+}
+
+func (r *TestRequest) Bind(obj any) error {
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) Route(key string) string {
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) RouteInt(key string) int {
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) RouteInt64(key string) int64 {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) Query(key string, defaultValue ...string) string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) QueryInt(key string, defaultValue ...int) int {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) QueryInt64(key string, defaultValue ...int64) int64 {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) QueryBool(key string, defaultValue ...bool) bool {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) QueryArray(key string) []string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) QueryMap(key string) map[string]string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) Queries() map[string]string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) Form(key string, defaultValue ...string) string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) Json(key string, defaultValue ...string) string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) Input(key string, defaultValue ...string) string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) InputArray(key string, defaultValue ...[]string) []string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) InputMap(key string, defaultValue ...map[string]string) map[string]string {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) InputInt(key string, defaultValue ...int) int {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) InputInt64(key string, defaultValue ...int64) int64 {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) InputBool(key string, defaultValue ...bool) bool {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) File(name string) (filesystem.File, error) {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) AbortWithStatus(code int) {}
+
+func (r *TestRequest) AbortWithStatusJson(code int, jsonObj any) {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) Next() {}
+
+func (r *TestRequest) Origin() *nethttp.Request {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) Validate(rules map[string]string, options ...validation.Option) (validation.Validator, error) {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestRequest) ValidateRequest(request contractshttp.FormRequest) (validation.Errors, error) {
+
+	panic("do not need to implement it")
+}
+
+type TestResponse struct {
+}
+
+func (r *TestResponse) Data(code int, contentType string, data []byte) {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) Download(filepath, filename string) {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) File(filepath string) {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) Header(key, value string) contractshttp.Response {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) Json(code int, obj any) {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) Origin() contractshttp.ResponseOrigin {
+	return &TestResponseOrigin{ctx: r}
+}
+
+func (r *TestResponse) Redirect(code int, location string) {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) String(code int, format string, values ...any) {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) Success() contractshttp.ResponseSuccess {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) Status(code int) contractshttp.ResponseStatus {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) Writer() nethttp.ResponseWriter {
+
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) Flush() {
+	panic("do not need to implement it")
+}
+
+func (r *TestResponse) View() contractshttp.ResponseView {
+	panic("do not need to implement it")
+}
+
+type TestResponseOrigin struct {
+	ctx *TestResponse
+}
+
+func (r *TestResponseOrigin) Body() *bytes.Buffer {
+	return bytes.NewBuffer([]byte("body"))
+}
+
+func (r *TestResponseOrigin) Header() nethttp.Header {
+	return nethttp.Header{
+		"Content-Type": []string{"text/plain; charset=utf-8"},
+	}
+}
+
+func (r *TestResponseOrigin) Size() int {
+	return r.Body().Len()
+}
+
+func (r *TestResponseOrigin) Status() int {
+	return 200
 }
