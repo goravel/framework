41bdad44aa238212ebe8b51081ed8b19495a569c
Wenbo Han <hwbrzzl@gmail.com>
2023-09-06 21:46:52 +0800

    feat: [#243] Optimize Response.Render return (#289)
    * feat: [#243] Optimize Response.Render return
    
    * Optimize mock

--- contracts/http/response.go
+++ contracts/http/response.go
@@ -7,9 +7,9 @@ import (
 
 type Json map[string]any
 
-//go:generate mockery --name=ContextResponse
+//go:generate mockery --name=Response
 type Response interface {
-	Render()
+	Render() error
 }
 
 //go:generate mockery --name=ContextResponse
