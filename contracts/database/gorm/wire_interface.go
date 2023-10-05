49965b2f2c74cd7280a61b2a9d90e231b8786dda
耗子 <i@haozi.net>
2023-10-05 12:26:24 +0800

    feat: move Gorm interface to contracts/database/gorm

--- contracts/database/gorm/wire_interface.go
+++ contracts/database/gorm/wire_interface.go
@@ -3,9 +3,15 @@ package gorm
 import (
 	"context"
 
+	gormio "gorm.io/gorm"
+
 	"github.com/goravel/framework/contracts/config"
 	"github.com/goravel/framework/database/gorm"
 )
+
+type Gorm interface {
+	Make() (*gormio.DB, error)
+}
 
 type Initialize interface {
 	InitializeGorm(config config.Config, connection string) *gorm.GormImpl
