388f6d011b6e8a5fcc6dcecdaa2bbd3117aef917
耗子 <i@haozi.net>
2023-11-05 22:28:38 +0800

    feat: optimize code

--- validation/validator.go
+++ validation/validator.go
@@ -2,6 +2,7 @@ package validation
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
 	"reflect"
 	"strings"
@@ -11,6 +12,14 @@ import (
 
 	httpvalidate "github.com/goravel/framework/contracts/validation"
 	"github.com/goravel/framework/support/str"
+)
+
+const (
+	errBindPointerOnly = "bind: must pass a pointer, not a value"
+	errBindStructOnly  = "bind: must pass a pointer to a struct"
+	errUnsupportedType = "bind: unsupported data source type"
+	errCastValueField  = "bind: cannot cast value to field"
+	errCastSliceElem   = "bind: cannot cast slice element value to field"
 )
 
 func init() {
@@ -34,202 +43,35 @@ func NewValidator(instance *validate.Validation, data validate.DataFace) *Valida
 func (v *Validator) Bind(ptr any) error {
 	val := reflect.ValueOf(ptr)
 	if val.Kind() != reflect.Ptr || val.IsNil() {
-		return errors.New("bind: must pass a pointer, not a value")
+		return errors.New(errBindPointerOnly)
 	}
 
 	val = val.Elem()
 	if val.Kind() != reflect.Struct {
-		return errors.New("bind: must pass a pointer to a struct")
+		return errors.New(errBindStructOnly)
 	}
 
 	dataSrc := v.data.Src()
 
-	// getFieldKey returns the key to use for the given field
-	getFieldKey := func(structField reflect.StructField) string {
-		formTag := structField.Tag.Get("form")
-		if len(formTag) == 0 {
-			formTag = structField.Tag.Get("json")
-		}
-		if len(formTag) == 0 {
-			formTag = structField.Name
-		}
-		return formTag
-	}
+	tagToFieldMap := createTagToFieldMap(val)
 
-	// setFieldValue sets the value of the given field
-	setFieldValue := func(field reflect.Value, structField reflect.StructField, value any) error {
-		var castedValue any
-		var err error
-
-		switch field.Kind() {
-		case reflect.String:
-			castedValue = cast.ToString(value)
-		case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-			castedValue, err = cast.ToInt64E(value)
-		case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-			castedValue, err = cast.ToUint64E(value)
-		case reflect.Bool:
-			castedValue, err = cast.ToBoolE(value)
-		case reflect.Float32, reflect.Float64:
-			castedValue, err = cast.ToFloat64E(value)
-		case reflect.Slice:
-			castedValue, err = cast.ToSliceE(value)
-		case reflect.Map:
-			castedValue, err = cast.ToStringMapE(value)
-		case reflect.Array:
-			castedValue, err = cast.ToSliceE(value)
-		default:
-			castedValue = value
-		}
-
-		if err != nil {
-			return errors.New("bind: cannot cast value to field " + structField.Name + ": " + err.Error())
-		}
-
-		if field.Kind() == reflect.Slice || field.Kind() == reflect.Array {
-			elemType := field.Type().Elem()
-			slice := reflect.MakeSlice(reflect.SliceOf(elemType), 0, len(castedValue.([]any)))
-			for _, v := range castedValue.([]any) {
-				elemVal := reflect.ValueOf(v)
-				if elemVal.Type().ConvertibleTo(elemType) {
-					slice = reflect.Append(slice, elemVal.Convert(elemType))
-				} else {
-					return errors.New("bind: cannot cast slice element value to field " + structField.Name)
-				}
-			}
-			field.Set(slice)
-		} else {
-			field.Set(reflect.ValueOf(castedValue).Convert(field.Type()))
-		}
-
-		return nil
-	}
-
-	// Check for url.Values
-	if values, ok := dataSrc.(url.Values); ok {
-		data := make(map[string]any)
-		for key, value := range values {
-			if len(value) > 0 {
-				data[key] = value[0]
-			}
-		}
-
+	switch data := dataSrc.(type) {
+	case url.Values:
 		formData, ok := v.data.(*validate.FormData)
 		if ok {
-			for key, value := range formData.Files {
-				data[key] = value
-			}
+			return bindFromURLValues(data, formData, tagToFieldMap)
 		}
-
-		for i := 0; i < val.NumField(); i++ {
-			field := val.Field(i)
-			if !field.CanSet() {
-				continue
-			}
-
-			structField := val.Type().Field(i)
-			formTag := getFieldKey(structField)
-
-			if value, ok := data[formTag]; ok {
-				if err := setFieldValue(field, structField, value); err != nil {
-					return err
-				}
-				continue
-			}
-			// Try lower case
-			if value, ok := data[strings.ToLower(formTag)]; ok {
-				if err := setFieldValue(field, structField, value); err != nil {
-					return err
-				}
-				continue
-			}
-			// Try snake case
-			if value, ok := data[str.Camel2Case(formTag)]; ok {
-				if err := setFieldValue(field, structField, value); err != nil {
-					return err
-				}
-				continue
-			}
+		return bindFromURLValues(data, nil, tagToFieldMap)
+	case map[string]any:
+		return bindFromMap(data, tagToFieldMap)
+	default:
+		val := reflect.Indirect(reflect.ValueOf(dataSrc))
+		if val.Kind() == reflect.Struct {
+			return bindFromStruct(val, tagToFieldMap)
+		} else {
+			return fmt.Errorf("%s: %s", errUnsupportedType, reflect.TypeOf(dataSrc).String())
 		}
-		return nil
 	}
-
-	// Check for map[string]any
-	if dataSrcMap, ok := dataSrc.(map[string]any); ok {
-		for i := 0; i < val.NumField(); i++ {
-			field := val.Field(i)
-			if !field.CanSet() {
-				continue
-			}
-
-			structField := val.Type().Field(i)
-			formTag := getFieldKey(structField)
-
-			if value, ok := dataSrcMap[formTag]; ok {
-				if err := setFieldValue(field, structField, value); err != nil {
-					return err
-				}
-				continue
-			}
-			// Try lower case
-			if value, ok := dataSrcMap[strings.ToLower(formTag)]; ok {
-				if err := setFieldValue(field, structField, value); err != nil {
-					return err
-				}
-				continue
-			}
-			// Try snake case
-			if value, ok := dataSrcMap[str.Camel2Case(formTag)]; ok {
-				if err := setFieldValue(field, structField, value); err != nil {
-					return err
-				}
-				continue
-			}
-		}
-		return nil
-	}
-
-	// Check for custom struct
-	dataSrcVal := reflect.ValueOf(dataSrc)
-	// struct may be a pointer
-	if dataSrcVal.Kind() == reflect.Ptr {
-		dataSrcVal = dataSrcVal.Elem()
-	}
-	if dataSrcVal.Kind() == reflect.Struct {
-		for i := 0; i < val.NumField(); i++ {
-			field := val.Field(i)
-			if !field.CanSet() {
-				continue
-			}
-
-			structField := val.Type().Field(i)
-			formTag := getFieldKey(structField)
-
-			if value := dataSrcVal.FieldByName(formTag); value.IsValid() && value.CanInterface() {
-				if err := setFieldValue(field, structField, value.Interface()); err != nil {
-					return err
-				}
-				continue
-			}
-			// Try lower case
-			if value := dataSrcVal.FieldByName(strings.ToLower(formTag)); value.IsValid() && value.CanInterface() {
-				if err := setFieldValue(field, structField, value.Interface()); err != nil {
-					return err
-				}
-				continue
-			}
-			// Try snake case
-			if value := dataSrcVal.FieldByName(str.Camel2Case(formTag)); value.IsValid() && value.CanInterface() {
-				if err := setFieldValue(field, structField, value.Interface()); err != nil {
-					return err
-				}
-				continue
-			}
-		}
-		return nil
-	}
-
-	return errors.New("bind: unsupported data source type " + reflect.TypeOf(dataSrc).String())
 }
 
 func (v *Validator) Errors() httpvalidate.Errors {
@@ -242,4 +84,127 @@ func (v *Validator) Errors() httpvalidate.Errors {
 
 func (v *Validator) Fails() bool {
 	return v.instance.IsFail()
+}
+
+func getFieldKey(structField reflect.StructField) string {
+	if formTag := structField.Tag.Get("form"); formTag != "" {
+		return formTag
+	}
+	if jsonTag := structField.Tag.Get("json"); jsonTag != "" {
+		return jsonTag
+	}
+	return structField.Name
+}
+
+func setFieldValue(field reflect.Value, value any) error {
+	_, err := castValueToType(field, value)
+	if err != nil {
+		return fmt.Errorf("%s %s: %w", errCastValueField, field.Type().String(), err)
+	}
+
+	field.Set(reflect.ValueOf(value))
+	return nil
+}
+
+func createTagToFieldMap(val reflect.Value) map[string]reflect.Value {
+	tagToFieldMap := make(map[string]reflect.Value)
+	typ := val.Type()
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		if !field.CanSet() {
+			continue
+		}
+		structField := typ.Field(i)
+		tag := getFieldKey(structField)
+		tagToFieldMap[tag] = field
+		tagToFieldMap[strings.ToLower(tag)] = field
+		tagToFieldMap[str.Camel2Case(tag)] = field
+	}
+	return tagToFieldMap
+}
+
+func bindFromURLValues(values url.Values, formData *validate.FormData, tagToFieldMap map[string]reflect.Value) error {
+	for tag, field := range tagToFieldMap {
+		if value, ok := values[tag]; ok && len(value) > 0 {
+			if err := setFieldValue(field, value[0]); err != nil {
+				return err
+			}
+		}
+		if formData != nil {
+			if value, ok := formData.Get(tag); ok {
+				if err := setFieldValue(field, value); err != nil {
+					return err
+				}
+			}
+		}
+	}
+	return nil
+}
+
+func bindFromMap(dataMap map[string]any, tagToFieldMap map[string]reflect.Value) error {
+	for tag, field := range tagToFieldMap {
+		if value, ok := dataMap[tag]; ok {
+			if err := setFieldValue(field, value); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func bindFromStruct(dataSrc reflect.Value, tagToFieldMap map[string]reflect.Value) error {
+	for tag, field := range tagToFieldMap {
+		if value := dataSrc.FieldByName(tag); value.IsValid() && value.CanInterface() {
+			if err := setFieldValue(field, value.Interface()); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func castValueToType(field reflect.Value, value any) (reflect.Value, error) {
+	var castedValue any
+	var err error
+
+	switch field.Kind() {
+	case reflect.String:
+		castedValue = cast.ToString(value)
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		castedValue, err = cast.ToInt64E(value)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		castedValue, err = cast.ToUint64E(value)
+	case reflect.Bool:
+		castedValue, err = cast.ToBoolE(value)
+	case reflect.Float32, reflect.Float64:
+		castedValue, err = cast.ToFloat64E(value)
+	case reflect.Slice:
+		castedValue, err = cast.ToSliceE(value)
+	case reflect.Map:
+		castedValue, err = cast.ToStringMapE(value)
+	case reflect.Array:
+		castedValue, err = cast.ToSliceE(value)
+	default:
+		castedValue = value
+	}
+
+	if err != nil {
+		return reflect.Value{}, fmt.Errorf("%s: %w", errCastValueField, err)
+	}
+
+	if field.Kind() == reflect.Slice || field.Kind() == reflect.Array {
+		elemType := field.Type().Elem()
+		slice := reflect.MakeSlice(reflect.SliceOf(elemType), 0, len(castedValue.([]any)))
+		for _, v := range castedValue.([]any) {
+			elemVal := reflect.ValueOf(v)
+			if elemVal.Type().ConvertibleTo(elemType) {
+				slice = reflect.Append(slice, elemVal.Convert(elemType))
+			} else {
+				return reflect.Value{}, fmt.Errorf("%s: %w", errCastSliceElem, err)
+			}
+		}
+		return slice, nil
+	}
+
+	return reflect.ValueOf(castedValue), nil
 }
