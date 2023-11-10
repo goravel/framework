376f1d9f92e10fba787fcd56cc82cf92e792b5b0
耗子 <i@haozi.net>
2023-11-10 15:51:43 +0800

    fix: Validator Bind can't cast value (#336)
    * feat: Optimize Validator Bind
    
    * feat: auto cast value
    
    * feat: optimize code
    
    * feat: add test cases
    
    * fix: lint
    
    * fix: lint
    
    * fix: support struct nesting
    
    * feat: add test case
    
    * feat: optimize type
    
    * refactor: use mapstructure to decode struct with cast
    
    * fix: remove unnecessary case
    
    * feat: make validate get field use form tag
    
    * feat: make validate get filtered and safe data
    
    * fix: lint
    
    * feat: remove unnecessary dataFace
    
    * feat: add cast test
    
    * feat: add error message
    
    * fix: tests
    
    * fix: tests
    
    * feat: Update validation/validator_test.go
    
    Co-authored-by: Wenbo Han <hwbrzzl@gmail.com>
    
    * fix: lint
    
    * feat: add more test case
    
    ---------
    
    Co-authored-by: Wenbo Han <hwbrzzl@gmail.com>

--- validation/validator_test.go
+++ validation/validator_test.go
@@ -11,6 +11,8 @@ import (
 
 	"github.com/gookit/validate"
 	"github.com/stretchr/testify/assert"
+
+	"github.com/goravel/framework/support/json"
 )
 
 func TestBind(t *testing.T) {
@@ -18,10 +20,9 @@ func TestBind(t *testing.T) {
 		A    string
 		B    int
 		C    string
+		D    *Data
 		File *multipart.FileHeader
 	}
-
-	request := buildRequest(t)
 
 	tests := []struct {
 		name       string
@@ -39,12 +40,28 @@ func TestBind(t *testing.T) {
 			},
 		},
 		{
+			name:  "success when data is map and key is int",
+			data:  validate.FromMap(map[string]any{"b": 1}),
+			rules: map[string]string{"b": "required"},
+			expectData: Data{
+				B: 1,
+			},
+		},
+		{
+			name:  "success when data is map and cast key",
+			data:  validate.FromMap(map[string]any{"b": "1"}),
+			rules: map[string]string{"b": "required"},
+			expectData: Data{
+				B: 1,
+			},
+		},
+		{
 			name:  "success when data is map, key is lowercase and has errors",
 			data:  validate.FromMap(map[string]any{"a": "aa", "c": "cc"}),
 			rules: map[string]string{"a": "required", "b": "required"},
 			expectData: Data{
-				A: "aa",
-				C: "cc",
+				A: "",
+				C: "",
 			},
 		},
 		{
@@ -88,6 +105,23 @@ func TestBind(t *testing.T) {
 			expectData: Data{},
 		},
 		{
+			name: "empty when data is struct and key is struct",
+			data: func() validate.DataFace {
+				data, err := validate.FromStruct(struct {
+					D *Data
+				}{
+					D: &Data{
+						A: "aa",
+					},
+				})
+				assert.Nil(t, err)
+
+				return data
+			}(),
+			rules:      map[string]string{"d.a": "required"},
+			expectData: Data{},
+		},
+		{
 			name: "success when data is get request",
 			data: func() validate.DataFace {
 				request, err := http.NewRequest(http.MethodGet, "/?a=aa", nil)
@@ -97,22 +131,38 @@ func TestBind(t *testing.T) {
 
 				return data
 			}(),
-			rules: map[string]string{"A": "required"},
+			rules: map[string]string{"a": "required"},
 			expectData: Data{
 				A: "aa",
 			},
 		},
 		{
-			name: "success when data is post request",
+			name: "success when data is get request and params is int",
 			data: func() validate.DataFace {
-				request, err := http.NewRequest(http.MethodGet, "/?a=aa", nil)
+				request, err := http.NewRequest(http.MethodGet, "/?b=1", nil)
 				assert.Nil(t, err)
 				data, err := validate.FromRequest(request)
 				assert.Nil(t, err)
 
 				return data
 			}(),
-			rules: map[string]string{"A": "required"},
+			rules: map[string]string{"b": "required"},
+			expectData: Data{
+				B: 1,
+			},
+		},
+		{
+			name: "success when data is post request",
+			data: func() validate.DataFace {
+				request, err := http.NewRequest(http.MethodPost, "/", bytes.NewBufferString(`{"a":"aa"}`))
+				request.Header.Set("Content-Type", "application/json")
+				assert.Nil(t, err)
+				data, err := validate.FromRequest(request)
+				assert.Nil(t, err)
+
+				return data
+			}(),
+			rules: map[string]string{"a": "required"},
 			expectData: Data{
 				A: "aa",
 			},
@@ -120,13 +170,15 @@ func TestBind(t *testing.T) {
 		{
 			name: "success when data is post request with body",
 			data: func() validate.DataFace {
+				request := buildRequest(t)
 				data, err := validate.FromRequest(request, 1)
 				assert.Nil(t, err)
 
 				return data
 			}(),
-			rules: map[string]string{"A": "required", "File": "required"},
+			rules: map[string]string{"a": "required", "file": "required"},
 			expectData: func() Data {
+				request := buildRequest(t)
 				_, fileHeader, _ := request.FormFile("file")
 				data := Data{
 					A:    "aa",
@@ -138,16 +190,19 @@ func TestBind(t *testing.T) {
 		},
 	}
 
+	validation := NewValidation()
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			validator := &Validator{data: test.data}
+			validator, err := validation.Make(test.data, test.rules)
+			assert.Nil(t, err)
 
 			var data Data
-			err := validator.Bind(&data)
+			err = validator.Bind(&data)
 			assert.Nil(t, test.expectErr, err)
 			assert.Equal(t, test.expectData.A, data.A)
 			assert.Equal(t, test.expectData.B, data.B)
 			assert.Equal(t, test.expectData.C, data.C)
+			assert.Equal(t, test.expectData.D, data.D)
 			assert.Equal(t, test.expectData.File == nil, data.File == nil)
 		})
 	}
@@ -182,6 +237,199 @@ func TestFails(t *testing.T) {
 		)
 		assert.Nil(t, err)
 		assert.Equal(t, test.expectRes, validator.Fails(), test.describe)
+	}
+}
+
+func TestCastValue(t *testing.T) {
+	type Data struct {
+		A string            `form:"a" json:"a"`
+		B int               `form:"b" json:"b"`
+		C int8              `form:"c" json:"c"`
+		D int16             `form:"d" json:"d"`
+		E int32             `form:"e" json:"e"`
+		F int64             `form:"f" json:"f"`
+		G uint              `form:"g" json:"g"`
+		H uint8             `form:"h" json:"h"`
+		I uint16            `form:"i" json:"i"`
+		J uint32            `form:"j" json:"j"`
+		K uint64            `form:"k" json:"k"`
+		L bool              `form:"l" json:"l"`
+		M float32           `form:"m" json:"m"`
+		N float64           `form:"n" json:"n"`
+		O []string          `form:"o" json:"o"`
+		P map[string]string `form:"p" json:"p"`
+	}
+
+	tests := []struct {
+		name       string
+		data       validate.DataFace
+		rules      map[string]string
+		expectData Data
+		expectErr  error
+	}{
+		{
+			name: "success without cast data",
+			data: func() validate.DataFace {
+				body := &Data{
+					A: "1",
+					B: 1,
+					C: 1,
+					D: 1,
+					E: 1,
+					F: 1,
+					G: 1,
+					H: 1,
+					I: 1,
+					J: 1,
+					K: 1,
+					L: true,
+					M: 1,
+					N: 1,
+					O: []string{"1"},
+					P: map[string]string{"a": "aa"},
+				}
+				jsonStr, err := json.Marshal(body)
+				assert.Nil(t, err)
+				request, err := http.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonStr))
+				request.Header.Set("Content-Type", "application/json")
+				assert.Nil(t, err)
+				data, err := validate.FromRequest(request)
+				assert.Nil(t, err)
+
+				return data
+			}(),
+			rules: map[string]string{
+				"a": "required",
+				"b": "required",
+				"c": "required",
+				"d": "required",
+				"e": "required",
+				"f": "required",
+				"g": "required",
+				"h": "required",
+				"i": "required",
+				"j": "required",
+				"k": "required",
+				"l": "required",
+				"m": "required",
+				"n": "required",
+				"o": "required",
+				"p": "required",
+			},
+			expectData: Data{
+				A: "1",
+				B: 1,
+				C: 1,
+				D: 1,
+				E: 1,
+				F: 1,
+				G: 1,
+				H: 1,
+				I: 1,
+				J: 1,
+				K: 1,
+				L: true,
+				M: 1,
+				N: 1,
+				O: []string{"1"},
+				P: map[string]string{"a": "aa"},
+			},
+		}, {
+			name: "success with cast data",
+			data: func() validate.DataFace {
+				body := map[string]any{
+					"a": 1,
+					"b": "1",
+					"c": "1",
+					"d": "1",
+					"e": "1",
+					"f": "1",
+					"g": "1",
+					"h": "1",
+					"i": "1",
+					"j": "1",
+					"k": "1",
+					"l": "true",
+					"m": "1",
+					"n": "1",
+					"o": []int{1},
+					"p": map[string]string{"a": "aa"},
+				}
+				jsonStr, err := json.Marshal(body)
+				assert.Nil(t, err)
+				request, err := http.NewRequest(http.MethodPost, "/", bytes.NewBuffer(jsonStr))
+				request.Header.Set("Content-Type", "application/json")
+				assert.Nil(t, err)
+				data, err := validate.FromRequest(request)
+				assert.Nil(t, err)
+
+				return data
+			}(),
+			rules: map[string]string{
+				"a": "required",
+				"b": "required",
+				"c": "required",
+				"d": "required",
+				"e": "required",
+				"f": "required",
+				"g": "required",
+				"h": "required",
+				"i": "required",
+				"j": "required",
+				"k": "required",
+				"l": "required",
+				"m": "required",
+				"n": "required",
+				"o": "required",
+				"p": "required",
+			},
+			expectData: Data{
+				A: "1",
+				B: 1,
+				C: 1,
+				D: 1,
+				E: 1,
+				F: 1,
+				G: 1,
+				H: 1,
+				I: 1,
+				J: 1,
+				K: 1,
+				L: true,
+				M: 1,
+				N: 1,
+				O: []string{"1"},
+				P: map[string]string{"a": "aa"},
+			},
+		},
+	}
+
+	validation := NewValidation()
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			validator, err := validation.Make(test.data, test.rules)
+			assert.Nil(t, err)
+
+			var data Data
+			err = validator.Bind(&data)
+			assert.Nil(t, test.expectErr, err)
+			assert.Equal(t, test.expectData.A, data.A)
+			assert.Equal(t, test.expectData.B, data.B)
+			assert.Equal(t, test.expectData.C, data.C)
+			assert.Equal(t, test.expectData.D, data.D)
+			assert.Equal(t, test.expectData.E, data.E)
+			assert.Equal(t, test.expectData.F, data.F)
+			assert.Equal(t, test.expectData.G, data.G)
+			assert.Equal(t, test.expectData.H, data.H)
+			assert.Equal(t, test.expectData.I, data.I)
+			assert.Equal(t, test.expectData.J, data.J)
+			assert.Equal(t, test.expectData.K, data.K)
+			assert.Equal(t, test.expectData.L, data.L)
+			assert.Equal(t, test.expectData.M, data.M)
+			assert.Equal(t, test.expectData.N, data.N)
+			assert.Equal(t, test.expectData.O, data.O)
+			assert.Equal(t, test.expectData.P, data.P)
+		})
 	}
 }
 
