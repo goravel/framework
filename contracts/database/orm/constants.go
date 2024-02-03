71235c66bebd78ffc40a440f86840a11a13caa31
kamandlou <ahkamandlou@gmail.com>
2024-02-03 19:44:36 +0330

    add DEPRECATED tag to DriverPostgresql

--- contracts/database/orm/constants.go
+++ contracts/database/orm/constants.go
@@ -1,7 +1,8 @@
 package orm
 
 const (
-	DriverMysql      Driver = "mysql"
+	DriverMysql Driver = "mysql"
+	// DriverPostgresql DEPRECATED, use DriverPostgres instead.
 	DriverPostgresql Driver = "postgresql"
 	DriverPostgres   Driver = "postgres"
 	DriverSqlite     Driver = "sqlite"
