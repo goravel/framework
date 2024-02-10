ade1be3fd6487181e1f854d80c0b6c7ac25d8547
kamandlou <ahkamandlou@gmail.com>
2024-02-10 19:58:19 +0330

    update test_utils.go

--- database/gorm/test_utils.go
+++ database/gorm/test_utils.go
@@ -740,6 +740,7 @@ func (r Table) createUserTableWithPrefixAndSingular(driver orm.Driver) string {
 CREATE TABLE goravel_user (
   id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   name varchar(255) NOT NULL,
+  bio varchar(255) DEFAULT NULL,
   avatar varchar(255) NOT NULL,
   created_at datetime(3) NOT NULL,
   updated_at datetime(3) NOT NULL,
@@ -754,6 +755,7 @@ CREATE TABLE goravel_user (
 CREATE TABLE goravel_user (
   id SERIAL PRIMARY KEY NOT NULL,
   name varchar(255) NOT NULL,
+  bio varchar(255) DEFAULT NULL,
   avatar varchar(255) NOT NULL,
   created_at timestamp NOT NULL,
   updated_at timestamp NOT NULL,
@@ -765,6 +767,7 @@ CREATE TABLE goravel_user (
 CREATE TABLE goravel_user (
   id integer PRIMARY KEY AUTOINCREMENT NOT NULL,
   name varchar(255) NOT NULL,
+  bio varchar(255) DEFAULT NULL,
   avatar varchar(255) NOT NULL,
   created_at datetime NOT NULL,
   updated_at datetime NOT NULL,
@@ -776,6 +779,7 @@ CREATE TABLE goravel_user (
 CREATE TABLE goravel_user (
   id bigint NOT NULL IDENTITY(1,1),
   name varchar(255) NOT NULL,
+  bio varchar(255) DEFAULT NULL,
   avatar varchar(255) NOT NULL,
   created_at datetime NOT NULL,
   updated_at datetime NOT NULL,
