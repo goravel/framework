810f391bc03f0cf21e8bd90476493dd58f9c4fbe
Kamandlou <ahkamandlou@gmail.com>
2024-04-15 17:16:21 +0330

    add Filter and Sum methods

--- support/collect/collect.go
+++ support/collect/collect.go
@@ -2,13 +2,25 @@ package collect
 
 import (
 	"github.com/samber/lo"
-	lop "github.com/samber/lo/parallel"
+	"golang.org/x/exp/constraints"
 )
 
+// Map manipulates a slice and transforms it to a slice of another type.
 func Map[T any, R any](collection []T, iteratee func(item T, index int) R) []R {
-	return lop.Map(collection, iteratee)
+	return lo.Map(collection, iteratee)
 }
 
+// Unique returns a duplicate-free version of an array, in which only the first occurrence of each element is kept.
 func Unique[T comparable](collection []T) []T {
 	return lo.Uniq(collection)
+}
+
+// Filter iterates over elements of collection, returning an array of all elements predicate returns truthy for.
+func Filter[V any](collection []V, predicate func(item V, index int) bool) []V {
+	return lo.Filter(collection, predicate)
+}
+
+// Sum sums the values in a collection. If collection is empty 0 is returned.
+func Sum[T constraints.Float | constraints.Integer | constraints.Complex](collection []T) T {
+	return lo.Sum(collection)
 }
--- support/collect/collect_test.go
+++ support/collect/collect_test.go
@@ -22,3 +22,16 @@ func TestUnique(t *testing.T) {
 	uniqValues := Unique([]int{1, 2, 2, 1})
 	assert.Equal(t, []int{1, 2}, uniqValues)
 }
+
+func TestFilter(t *testing.T) {
+	even := Filter([]int{1, 2, 3, 4}, func(x int, index int) bool {
+		return x%2 == 0
+	})
+	assert.Equal(t, []int{2, 4}, even)
+}
+
+func TestSum(t *testing.T) {
+	list := []int{1, 2, 3, 4, 5}
+	sum := Sum(list)
+	assert.Equal(t, 15, sum)
+}
