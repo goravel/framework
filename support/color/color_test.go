140fc7bfd9fe197239ebc88424eb96ae9719d0f3
kkumar-gcc <krishkumar9352@gmail.com>
2024-05-20 11:57:30 +0530

    add test cases for color

--- support/color/color_test.go
+++ support/color/color_test.go
@@ -7,14 +7,48 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestColor(t *testing.T) {
-	t.Run("TestColor", func(t *testing.T) {
-		red := New(FgRed)
+func captureOutput(f func()) string {
+	return ""
+}
 
-		assert.Equal(t, fmt.Sprintf("\x1b[%dm%s\x1b[0m", FgRed, "test"), red.Sprint("test"))
-		assert.Equal(t, fmt.Sprintf("\x1b[%dm%s\x1b[0m\n\x1b[%dm\x1b[0m", FgRed, "test", FgRed), red.Sprintln("test"))
-		assert.Equal(t, fmt.Sprintf("\x1b[%dm%s\x1b[0m", FgRed, "test"), red.Sprintf("%s", "test"))
-		assert.Equal(t, fmt.Sprintf("\x1b[%dm%s\x1b[0m\n\u001B[%dm\u001B[0m", FgRed, "test", FgRed), red.Sprintfln("%s", "test"))
-	})
+func TestColors(t *testing.T) {
+	colors := map[string]Printer{
+		"FgBlack":        Black(),
+		"FgRed":          Red(),
+		"FgGreen":        Green(),
+		"FgYellow":       Yellow(),
+		"FgBlue":         Blue(),
+		"FgMagenta":      Magenta(),
+		"FgCyan":         Cyan(),
+		"FgWhite":        White(),
+		"FgDefault":      Normal(),
+		"FgDarkGray":     FgDarkGray,
+		"FgLightRed":     FgLightRed,
+		"FgLightGreen":   FgLightGreen,
+		"FgLightYellow":  FgLightYellow,
+		"FgLightBlue":    FgLightBlue,
+		"FgLightMagenta": FgLightMagenta,
+		"FgLightCyan":    FgLightCyan,
+		"FgLightWhite":   FgLightWhite,
+		"FgGray":         Gray(),
+	}
 
+	for name, color := range colors {
+		t.Run(name, func(t *testing.T) {
+			testString := "test"
+			format := "%s"
+			expected := fmt.Sprintf("\x1b[%dm%s\x1b[0m", color, testString)
+			expectedLn := fmt.Sprintf("\x1b[%dm%s\x1b[0m\n\x1b[%dm\x1b[0m", color, testString, color)
+
+			assert.Equal(t, expected, color.Sprint(testString))
+			assert.Equal(t, expectedLn, color.Sprintln(testString))
+			assert.Equal(t, expected, color.Sprintf(format, testString))
+			assert.Equal(t, expectedLn, color.Sprintfln(format, testString))
+
+			//assert.Equal(t, expected, captureOutput(func() { print(color.Print(testString)) }))
+			//assert.Equal(t, expectedLn, captureOutput(func() { println(color.Println(testString)) }))
+			//assert.Equal(t, expected, captureOutput(func() { print(color.Printf(format, testString)) }))
+			//assert.Equal(t, expectedLn, captureOutput(func() { println(color.Printfln(format, testString)) }))
+		})
+	}
 }
