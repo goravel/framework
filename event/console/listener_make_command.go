e9c51169ea886804ee69f87f9d47912fd5494613
kkumar-gcc <krishkumar9352@gmail.com>
2024-05-21 09:38:57 +0530

    optimize make:listener command

--- event/console/listener_make_command.go
+++ event/console/listener_make_command.go
@@ -44,16 +44,26 @@ func (receiver *ListenerMakeCommand) Extend() command.Extend {
 func (receiver *ListenerMakeCommand) Handle(ctx console.Context) error {
 	name := ctx.Argument(0)
 	if name == "" {
-		return errors.New("Not enough arguments (missing: name) ")
+		var err error
+		name, err = ctx.Ask("Enter the listener name", console.AskOption{
+			Validate: func(s string) error {
+				if s == "" {
+					return errors.New("the listener name cannot be empty")
+				}
+
+				return nil
+			},
+		})
+		if err != nil {
+			return err
+		}
 	}
 
 	force := ctx.OptionBool("force")
 	path := receiver.getPath(name)
-	if !force {
-		if file.Exists(path) {
-			color.Red().Println("The listener already exists. Use the --force flag to overwrite")
-			return nil
-		}
+	if !force && file.Exists(path) {
+		color.Red().Println("The listener already exists. Use the --force flag to overwrite")
+		return nil
 	}
 
 	if err := file.Create(path, receiver.populateStub(receiver.getStub(), name)); err != nil {
--- event/console/listener_make_command_test.go
+++ event/console/listener_make_command_test.go
@@ -1,11 +1,15 @@
 package console
 
 import (
+	"errors"
+	"io"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 
 	consolemocks "github.com/goravel/framework/mocks/console"
+	"github.com/goravel/framework/support/color"
 	"github.com/goravel/framework/support/file"
 )
 
@@ -13,15 +17,24 @@ func TestListenerMakeCommand(t *testing.T) {
 	listenerMakeCommand := &ListenerMakeCommand{}
 	mockContext := &consolemocks.Context{}
 	mockContext.On("Argument", 0).Return("").Once()
+	mockContext.On("Ask", "Enter the listener name", mock.Anything).Return("", errors.New("the listener name cannot be empty")).Once()
 	err := listenerMakeCommand.Handle(mockContext)
-	assert.EqualError(t, err, "Not enough arguments (missing: name) ")
+	assert.EqualError(t, err, "the listener name cannot be empty")
 
 	mockContext.On("Argument", 0).Return("GoravelListen").Once()
+	mockContext.On("OptionBool", "force").Return(false).Once()
 	err = listenerMakeCommand.Handle(mockContext)
 	assert.Nil(t, err)
 	assert.True(t, file.Exists("app/listeners/goravel_listen.go"))
 
+	mockContext.On("Argument", 0).Return("GoravelListen").Once()
+	mockContext.On("OptionBool", "force").Return(false).Once()
+	assert.Contains(t, color.CaptureOutput(func(w io.Writer) {
+		assert.Nil(t, listenerMakeCommand.Handle(mockContext))
+	}), "The listener already exists. Use the --force flag to overwrite")
+
 	mockContext.On("Argument", 0).Return("Goravel/Listen").Once()
+	mockContext.On("OptionBool", "force").Return(false).Once()
 	err = listenerMakeCommand.Handle(mockContext)
 	assert.Nil(t, err)
 	assert.True(t, file.Exists("app/listeners/Goravel/listen.go"))
