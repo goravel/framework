9bb7680050921ce6101b38a7d40e829435e344e5
kkumar-gcc <krishkumar9352@gmail.com>
2024-05-23 12:24:35 +0530

    add customization for channel

--- log/application.go
+++ log/application.go
@@ -13,25 +13,15 @@ import (
 type Application struct {
 	log.Writer
 	instance *logrus.Logger
+	config   config.Config
 }
 
 func NewApplication(config config.Config) *Application {
-	instance := logrus.New()
-	instance.SetLevel(logrus.DebugLevel)
-
-	if config != nil {
-		if logging := config.GetString("logging.default"); logging != "" {
-			if err := registerHook(config, instance, logging); err != nil {
-				color.Red().Println("Init facades.Log error: " + err.Error())
-
-				return nil
-			}
-		}
-	}
-
+	instance := newLogger(config, config.GetString("logging.default"))
 	return &Application{
 		instance: instance,
 		Writer:   NewWriter(instance.WithContext(context.Background())),
+		config:   config,
 	}
 }
 
@@ -42,4 +32,57 @@ func (r *Application) WithContext(ctx context.Context) log.Writer {
 	default:
 		return r.Writer
 	}
+}
+
+func (r *Application) Channel(channel string) log.Writer {
+	if channel == "" || r.config == nil {
+		return r.Writer
+	}
+
+	instance := newLogger(r.config, channel)
+	return NewWriter(instance.WithContext(context.Background()))
+}
+
+func (r *Application) Stack(channels []string) log.Writer {
+	if r.config == nil {
+		return r.Writer
+	}
+
+	instance := logrus.New()
+	instance.SetLevel(logrus.DebugLevel)
+
+	for _, channel := range channels {
+		if channel == "" {
+			continue
+		}
+
+		channelPath := "logging.channels." + channel
+		driver := r.config.GetString(channelPath + ".driver")
+
+		if driver == log.StackDriver {
+			color.Red().Println("stack driver can't include self channel")
+			return nil
+		}
+
+		if err := registerHook(r.config, instance, channel); err != nil {
+			color.Red().Println("Init facades.Log error: " + err.Error())
+			return nil
+		}
+	}
+
+	return NewWriter(instance.WithContext(context.Background()))
+}
+
+func newLogger(config config.Config, channel string) *logrus.Logger {
+	instance := logrus.New()
+	instance.SetLevel(logrus.DebugLevel)
+
+	if channel != "" && config != nil {
+		if err := registerHook(config, instance, channel); err != nil {
+			color.Red().Println("Init facades.Log error: " + err.Error())
+			return nil
+		}
+	}
+
+	return instance
 }
