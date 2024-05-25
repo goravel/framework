dc9f29e57216aaba6158ede58a2de26781ffaa76
耗子 <i@haozi.net>
2024-05-25 11:55:43 +0800

    feat: translation use executable path

--- translation/service_provider.go
+++ translation/service_provider.go
@@ -5,6 +5,7 @@ import (
 	"path/filepath"
 
 	"github.com/goravel/framework/contracts/foundation"
+	"github.com/goravel/framework/support/path"
 )
 
 const Binding = "goravel.translation"
@@ -14,11 +15,16 @@ type ServiceProvider struct {
 
 func (translation *ServiceProvider) Register(app foundation.Application) {
 	app.BindWith(Binding, func(app foundation.Application, parameters map[string]any) (any, error) {
+		executable, err := path.Executable()
+		if err != nil {
+			return nil, err
+		}
+
 		config := app.MakeConfig()
 		logger := app.MakeLog()
 		locale := config.GetString("app.locale")
 		fallback := config.GetString("app.fallback_locale")
-		loader := NewFileLoader([]string{filepath.Join("lang")})
+		loader := NewFileLoader([]string{filepath.Join(executable, "lang")})
 		trans := NewTranslator(parameters["ctx"].(context.Context), loader, locale, fallback, logger)
 		trans.SetLocale(locale)
 		return trans, nil
