66831d9574ae35c7b621583f1148828fed5386ae
kkumar-gcc <krishkumar9352@gmail.com>
2024-05-27 07:12:44 +0530

    add default driver for json

--- contracts/support/json.go
+++ contracts/support/json.go
@@ -3,6 +3,10 @@ package support
 type Json interface {
 	// Marshal returns the JSON encoding of v.
 	Marshal(any) ([]byte, error)
+	// MarshalString returns the JSON encoding of v as a string.
+	MarshalString(any) (string, error)
 	// Unmarshal parses the JSON-encoded data and stores the result in the value pointed to by v.
 	Unmarshal([]byte, any) error
+	// UnmarshalString parses the JSON-encoded data and stores the result in the value pointed to by v.
+	UnmarshalString(string, any) error
 }
--- support/json/json_x86.go
+++ support/json/json_x86.go
@@ -3,8 +3,41 @@
 package json
 
 import (
+	"encoding/json"
+
 	"github.com/bytedance/sonic"
+
+	"github.com/goravel/framework/contracts/support"
 )
+
+type Json struct {
+	marshal   func(any) ([]byte, error)
+	unmarshal func([]byte, any) error
+}
+
+func New() support.Json {
+	return &Json{
+		marshal:   json.Marshal,
+		unmarshal: json.Unmarshal,
+	}
+}
+
+func (j *Json) Marshal(v any) ([]byte, error) {
+	return j.marshal(v)
+}
+
+func (j *Json) MarshalString(v any) (string, error) {
+	s, err := j.marshal(v)
+	return string(s), err
+}
+
+func (j *Json) Unmarshal(data []byte, v any) error {
+	return j.unmarshal(data, v)
+}
+
+func (j *Json) UnmarshalString(data string, v any) error {
+	return j.unmarshal([]byte(data), v)
+}
 
 // Marshal is a wrapper of sonic.Marshal.
 // Marshal 是 sonic.Marshal 的包装器。
