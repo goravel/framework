cbda086a6e5dfff5eb955ee074d82c481af6ec92
kkumar-gcc <krishkumar9352@gmail.com>
2024-05-27 18:52:50 +0530

    add string method for json

--- support/json/json.go
+++ support/json/json.go
@@ -11,3 +11,14 @@ func Marshal(v any) ([]byte, error) {
 func Unmarshal(data []byte, v any) error {
 	return foundation.App.GetJson().Unmarshal(data, v)
 }
+
+// MarshalString serializes the given value to a JSON-encoded string.
+func MarshalString(v any) (string, error) {
+	s, err := Marshal(v)
+	return string(s), err
+}
+
+// UnmarshalString deserializes the given JSON-encoded string into the provided value.
+func UnmarshalString(data string, v any) error {
+	return Unmarshal([]byte(data), v)
+}
