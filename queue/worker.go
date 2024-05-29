bddcb8bcc28a6e25ef53dd4ab88e282a0bbbc65a
耗子 <i@haozi.net>
2024-05-30 01:43:04 +0800

    fix: remove signal listen

--- queue/worker.go
+++ queue/worker.go
@@ -2,10 +2,6 @@ package queue
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"sync"
-	"syscall"
 	"time"
 
 	"github.com/goravel/framework/contracts/database/orm"
@@ -19,8 +15,7 @@ type Worker struct {
 	failedJobs    orm.Query
 	queue         string
 	failedJobChan chan FailedJob
-	sigChan       chan os.Signal
-	quitChan      chan struct{}
+	isShutdown    bool
 }
 
 func NewWorker(config *Config, concurrent int, connection string, queue string, job *JobImpl) *Worker {
@@ -31,8 +26,6 @@ func NewWorker(config *Config, concurrent int, connection string, queue string, 
 		failedJobs:    config.FailedJobsQuery(),
 		queue:         queue,
 		failedJobChan: make(chan FailedJob),
-		sigChan:       make(chan os.Signal, 1),
-		quitChan:      make(chan struct{}),
 	}
 }
 
@@ -45,48 +38,33 @@ func (r *Worker) Run() error {
 		return fmt.Errorf("queue %s driver not need run", r.queue)
 	}
 
-	var wg sync.WaitGroup
-	signal.Notify(r.sigChan, syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGTERM)
-
 	for i := 0; i < r.concurrent; i++ {
-		wg.Add(1)
 		go func() {
-			defer wg.Done()
 			for {
-				select {
-				case <-r.quitChan:
+				if r.isShutdown {
 					return
-				default:
-					job, args, err := driver.Pop(r.queue)
-					if err != nil {
-						// This error not need to be reported.
-						// It is usually caused by the queue being empty.
-						time.Sleep(1 * time.Second)
-						continue
-					}
+				}
 
-					err = r.job.Call(job.Signature(), args)
-					if err != nil {
-						r.failedJobChan <- FailedJob{
-							Queue:     r.queue,
-							Signature: job.Signature(),
-							Payloads:  args,
-							Exception: err.Error(),
-							FailedAt:  carbon.DateTime{Carbon: carbon.Now()},
-						}
+				job, args, err := driver.Pop(r.queue)
+				if err != nil {
+					// This error not need to be reported.
+					// It is usually caused by the queue being empty.
+					time.Sleep(1 * time.Second)
+					continue
+				}
+
+				if err = r.job.Call(job.Signature(), args); err != nil {
+					r.failedJobChan <- FailedJob{
+						Queue:     r.queue,
+						Signature: job.Signature(),
+						Payloads:  args,
+						Exception: err.Error(),
+						FailedAt:  carbon.DateTime{Carbon: carbon.Now()},
 					}
 				}
 			}
 		}()
 	}
-
-	go func() {
-		sig := <-r.sigChan
-		fmt.Printf("Received signal: %s, shutting down queue %s...\n", sig, r.queue)
-		close(r.quitChan)
-		wg.Wait()
-		close(r.failedJobChan)
-	}()
 
 	go func() {
 		for job := range r.failedJobChan {
@@ -98,6 +76,6 @@ func (r *Worker) Run() error {
 }
 
 func (r *Worker) Shutdown() error {
-	r.sigChan <- syscall.SIGTERM
+	r.isShutdown = true
 	return nil
 }
