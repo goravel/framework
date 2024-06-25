b37b0ef9c28d7a1831fb4cfc2fd9c3b02f81234c
耗子 <i@haozi.net>
2024-06-26 00:19:49 +0800

    fix: test

--- schedule/application.go
+++ schedule/application.go
@@ -39,7 +39,7 @@ func (app *Application) Command(command string) schedule.Event {
 
 func (app *Application) Register(events []schedule.Event) {
 	if app.cron == nil {
-		app.cron = cron.New(cron.WithLogger(NewLogger(app.log, app.logInfo)))
+		app.cron = cron.New(cron.WithLogger(NewLogger(app.log, app.debug)))
 	}
 
 	app.addEvents(events)
@@ -51,11 +51,11 @@ func (app *Application) Run() {
 
 func (app *Application) addEvents(events []schedule.Event) {
 	for _, event := range events {
-		chain := cron.NewChain(cron.Recover(NewLogger(app.log, app.logInfo)))
+		chain := cron.NewChain(cron.Recover(NewLogger(app.log, app.debug)))
 		if event.GetDelayIfStillRunning() {
-			chain = cron.NewChain(cron.DelayIfStillRunning(NewLogger(app.log, app.logInfo)), cron.Recover(NewLogger(app.log, app.logInfo)))
+			chain = cron.NewChain(cron.DelayIfStillRunning(NewLogger(app.log, app.debug)), cron.Recover(NewLogger(app.log, app.debug)))
 		} else if event.GetSkipIfStillRunning() {
-			chain = cron.NewChain(cron.SkipIfStillRunning(NewLogger(app.log, app.logInfo)), cron.Recover(NewLogger(app.log, app.logInfo)))
+			chain = cron.NewChain(cron.SkipIfStillRunning(NewLogger(app.log, app.debug)), cron.Recover(NewLogger(app.log, app.debug)))
 		}
 		_, err := app.cron.AddJob(event.GetCron(), chain.Then(app.getJob(event)))
 
