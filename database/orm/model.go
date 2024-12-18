f2bdffab47a8dd3b0d81003dd114647f43c00c0b
Bowen <hwbrzzl@gmail.com>
2024-12-18 23:46:13 +0800

    cancel test

--- database/orm/model.go
+++ database/orm/model.go
@@ -15,7 +15,7 @@ type Model struct {
 }
 
 type SoftDeletes struct {
-	Deleted gorm.DeletedAt `gorm:"column:deleted_at" json:"deleted_at"`
+	DeletedAt gorm.DeletedAt `gorm:"column:deleted_at" json:"deleted_at"`
 }
 
 type Timestamps struct {
