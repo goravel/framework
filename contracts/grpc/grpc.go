dc45fb98fc4423610995f875e958c615d41090f1
耗子 <i@haozi.net>
2025-02-05 20:57:33 +0800

    feat: grpc graceful shutdown (#850)
    * feat: grpc graceful shutdown
    
    * chore: update mocks
    
    * feat: optimize test

--- contracts/grpc/grpc.go
+++ contracts/grpc/grpc.go
@@ -2,17 +2,22 @@ package grpc
 
 import (
 	"context"
+	"net"
 
 	"google.golang.org/grpc"
 )
 
 type Grpc interface {
+	// Client gets the gRPC client instance.
+	Client(ctx context.Context, name string) (*grpc.ClientConn, error)
+	// Listen starts the gRPC server with the given listener.
+	Listen(l net.Listener) error
 	// Run starts the gRPC server.
 	Run(host ...string) error
 	// Server gets the gRPC server instance.
 	Server() *grpc.Server
-	// Client gets the gRPC client instance.
-	Client(ctx context.Context, name string) (*grpc.ClientConn, error)
+	// Shutdown stops the gRPC server.
+	Shutdown(force ...bool)
 	// UnaryServerInterceptors sets the gRPC server interceptors.
 	UnaryServerInterceptors([]grpc.UnaryServerInterceptor)
 	// UnaryClientInterceptorGroups sets the gRPC client interceptor groups.
