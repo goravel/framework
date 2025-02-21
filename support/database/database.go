05a7f44ca965582690366be5dc9601b0151f3113
Wenbo Han <hwbrzzl@gmail.com>
2025-02-21 17:07:56 +0800

    fix: Problem with submodels id string (#903)

--- support/database/database.go
+++ support/database/database.go
@@ -3,8 +3,6 @@ package database
 import (
 	"reflect"
 	"strings"
-
-	"github.com/spf13/cast"
 )
 
 func GetID(dest any) any {
@@ -15,41 +13,28 @@ func GetID(dest any) any {
 	t := reflect.TypeOf(dest)
 	v := reflect.ValueOf(dest)
 
-	if t.Kind() == reflect.Pointer {
-		return GetIDByReflect(t.Elem(), v.Elem())
-	}
-
 	return GetIDByReflect(t, v)
 }
 
 func GetIDByReflect(t reflect.Type, v reflect.Value) any {
+	if t.Kind() == reflect.Pointer {
+		t = t.Elem()
+		v = v.Elem()
+	}
+
 	for i := 0; i < t.NumField(); i++ {
 		if !t.Field(i).IsExported() {
 			continue
 		}
 		if strings.Contains(t.Field(i).Tag.Get("gorm"), "primaryKey") {
-			id := v.Field(i).Interface()
-			if cast.ToString(id) == "" && cast.ToInt(id) == 0 {
+			if v.Field(i).IsZero() {
 				return nil
 			}
 
-			return id
+			return v.Field(i).Interface()
 		}
-		if v.Field(i).Type().Kind() == reflect.Struct {
-			structField := v.Field(i).Type()
-			for j := 0; j < structField.NumField(); j++ {
-				if !structField.Field(j).IsExported() {
-					continue
-				}
-				if strings.Contains(structField.Field(j).Tag.Get("gorm"), "primaryKey") {
-					id := v.Field(i).Field(j).Interface()
-					if cast.ToString(id) == "" || cast.ToInt(id) == 0 {
-						return nil
-					}
-
-					return id
-				}
-			}
+		if t.Field(i).Type.Kind() == reflect.Struct && t.Field(i).Anonymous {
+			return GetIDByReflect(t.Field(i).Type, v.Field(i))
 		}
 	}
 
