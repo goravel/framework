850582198a9609e4fd0590dddd916627e4fb25c6
hwbrzzl <24771476+hwbrzzl@users.noreply.github.com>
2025-03-03 04:04:29 +0000

    chore: update mocks

--- mocks/database/db/Query.go
+++ mocks/database/db/Query.go
@@ -20,6 +20,61 @@ func (_m *Query) EXPECT() *Query_Expecter {
 	return &Query_Expecter{mock: &_m.Mock}
 }
 
+// Count provides a mock function with no fields
+func (_m *Query) Count() (int64, error) {
+	ret := _m.Called()
+
+	if len(ret) == 0 {
+		panic("no return value specified for Count")
+	}
+
+	var r0 int64
+	var r1 error
+	if rf, ok := ret.Get(0).(func() (int64, error)); ok {
+		return rf()
+	}
+	if rf, ok := ret.Get(0).(func() int64); ok {
+		r0 = rf()
+	} else {
+		r0 = ret.Get(0).(int64)
+	}
+
+	if rf, ok := ret.Get(1).(func() error); ok {
+		r1 = rf()
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
+// Query_Count_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'Count'
+type Query_Count_Call struct {
+	*mock.Call
+}
+
+// Count is a helper method to define mock.On call
+func (_e *Query_Expecter) Count() *Query_Count_Call {
+	return &Query_Count_Call{Call: _e.mock.On("Count")}
+}
+
+func (_c *Query_Count_Call) Run(run func()) *Query_Count_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		run()
+	})
+	return _c
+}
+
+func (_c *Query_Count_Call) Return(_a0 int64, _a1 error) *Query_Count_Call {
+	_c.Call.Return(_a0, _a1)
+	return _c
+}
+
+func (_c *Query_Count_Call) RunAndReturn(run func() (int64, error)) *Query_Count_Call {
+	_c.Call.Return(run)
+	return _c
+}
+
 // Delete provides a mock function with no fields
 func (_m *Query) Delete() (*db.Result, error) {
 	ret := _m.Called()
