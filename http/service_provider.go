c6d1b49989a964c7274acc11b3c21c13e7e6452f
Bowen <hwbrzzl@gmail.com>
2025-03-16 13:07:10 +0800

    sync

--- http/service_provider.go
+++ http/service_provider.go
@@ -1,13 +1,17 @@
 package http
 
 import (
+	"time"
+
 	"github.com/goravel/framework/contracts"
 	"github.com/goravel/framework/contracts/cache"
-	consolecontract "github.com/goravel/framework/contracts/console"
+	contractsconsole "github.com/goravel/framework/contracts/console"
 	"github.com/goravel/framework/contracts/foundation"
 	"github.com/goravel/framework/contracts/http"
+	contractsclient "github.com/goravel/framework/contracts/http/client"
 	"github.com/goravel/framework/contracts/log"
 	"github.com/goravel/framework/errors"
+	"github.com/goravel/framework/http/client"
 	"github.com/goravel/framework/http/console"
 )
 
@@ -20,16 +24,37 @@ var (
 	JsonFacade        foundation.Json
 )
 
-func (http *ServiceProvider) Register(app foundation.Application) {
+func (r *ServiceProvider) Register(app foundation.Application) {
 	app.Singleton(contracts.BindingRateLimiter, func(app foundation.Application) (any, error) {
 		return NewRateLimiter(), nil
 	})
 	app.Singleton(contracts.BindingView, func(app foundation.Application) (any, error) {
 		return NewView(), nil
 	})
+	app.Bind(contracts.BindingHttp, func(app foundation.Application) (any, error) {
+		c := app.MakeConfig()
+		if c == nil {
+			return nil, errors.ConfigFacadeNotSet.SetModule(errors.ModuleHttp)
+		}
+
+		j := app.GetJson()
+		if j == nil {
+			return nil, errors.JSONParserNotSet.SetModule(errors.ModuleHttp)
+		}
+
+		config := &contractsclient.Config{
+			Timeout:             c.GetDuration("http.client.timeout", 30*time.Second),
+			BaseUrl:             c.GetString("http.client.base_url"),
+			MaxIdleConns:        c.GetInt("http.client.max_idle_conns"),
+			MaxIdleConnsPerHost: c.GetInt("http.client.max_idle_conns_per_host"),
+			MaxConnsPerHost:     c.GetInt("http.client.max_conns_per_host"),
+			IdleConnTimeout:     c.GetDuration("http.client.idle_conn_timeout"),
+		}
+		return client.NewRequest(config, j), nil
+	})
 }
 
-func (http *ServiceProvider) Boot(app foundation.Application) {
+func (r *ServiceProvider) Boot(app foundation.Application) {
 	CacheFacade = app.MakeCache()
 	if CacheFacade == nil {
 		panic(errors.CacheFacadeNotSet.SetModule(errors.ModuleHttp))
@@ -50,11 +75,11 @@ func (http *ServiceProvider) Boot(app foundation.Application) {
 		panic(errors.JSONParserNotSet.SetModule(errors.ModuleHttp))
 	}
 
-	http.registerCommands(app)
+	r.registerCommands(app)
 }
 
 func (http *ServiceProvider) registerCommands(app foundation.Application) {
-	app.Commands([]consolecontract.Command{
+	app.Commands([]contractsconsole.Command{
 		&console.RequestMakeCommand{},
 		&console.ControllerMakeCommand{},
 		&console.MiddlewareMakeCommand{},
