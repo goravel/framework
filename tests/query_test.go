3dbe70ce859fff624552cf3eceed6f90e43d370d
almas1992 <almas1992@126.com>
2025-04-02 15:57:29 +0800

    add complex test cases

--- tests/query_test.go
+++ tests/query_test.go
@@ -3215,8 +3215,8 @@ func (s *QueryTestSuite) TestWhere() {
 
 			var user5 User
 			s.Nil(query.Query().Where(func(query contractsorm.Query) contractsorm.Query {
-				return query.Where("name = ?", "where_user")
-			}).Find(&user5))
+				return query.Where("name = ?", "where_user").OrWhere("name", "where_user1")
+			}).Where("avatar", "where_avatar").Find(&user5))
 			s.True(user5.ID > 0)
 		})
 	}
