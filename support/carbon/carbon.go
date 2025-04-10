3ef11931bbc703c08682ed5d8e2533b69f3f1eeb
gouguoyin <245629560@qq.com>
2025-04-10 14:23:40 +0800

    feat: Refer to `https://pkg.go.dev/time` for unified comment format

--- support/carbon/carbon.go
+++ support/carbon/carbon.go
@@ -8,144 +8,143 @@ import (
 
 type Carbon = carbon.Carbon
 
-// SetTestNow Set the test time. Remember to unset after used.
+// SetTestNow sets the test time. Remember to unset after used.
 func SetTestNow(testTime Carbon) {
 	carbon.SetTestNow(testTime)
 }
 
-// UnsetTestNow Unset the test time.
+// UnsetTestNow unsets the test time.
 // Deprecated: it will be deprecated in the future, please use 'CleanTestNow' instead
 func UnsetTestNow() {
 	CleanTestNow()
 }
 
-// CleanTestNow clean the test time.
+// CleanTestNow cleans the test time.
 func CleanTestNow() {
 	carbon.CleanTestNow()
 }
 
-// IsTestNow Determine if the test now time is set.
+// IsTestNow determines if the test now time is set.
 func IsTestNow() bool {
 	return carbon.IsTestNow()
 }
 
 // SetTimezone sets timezone.
-// 设置时区
 func SetTimezone(timezone string) {
 	carbon.SetTimezone(timezone)
 }
 
-// Now return a Carbon object of now.
+// Now returns a Carbon object of now.
 func Now(timezone ...string) Carbon {
 	return carbon.Now(timezone...)
 }
 
-// Parse return a Carbon object of given value.
+// Parse returns a Carbon object of given value.
 func Parse(value string, timezone ...string) Carbon {
 	return carbon.Parse(value, timezone...)
 }
 
-// ParseByFormat return a Carbon object of given value and format.
+// ParseByFormat returns a Carbon object by a confirmed format.
 func ParseByFormat(value, format string, timezone ...string) Carbon {
 	return carbon.ParseByFormat(value, format, timezone...)
 }
 
-// ParseByLayout return a Carbon object of given value and layout.
+// ParseByLayout returns a Carbon object by a confirmed layout.
 func ParseByLayout(value, layout string, timezone ...string) Carbon {
 	return carbon.ParseByLayout(value, layout, timezone...)
 }
 
-// ParseWithFormats parses time string with formats as a Carbon instance.
+// ParseWithFormats returns a Carbon object with multiple fuzzy formats.
 func ParseWithFormats(value string, formats []string, timezone ...string) Carbon {
 	return carbon.ParseWithFormats(value, formats, timezone...)
 }
 
-// ParseWithLayouts parses time string with layouts as a Carbon instance.
+// ParseWithLayouts returns a Carbon object with multiple fuzzy layouts.
 func ParseWithLayouts(value string, layouts []string, timezone ...string) Carbon {
 	return carbon.ParseWithLayouts(value, layouts, timezone...)
 }
 
-// FromTimestamp return a Carbon object of given timestamp.
+// FromTimestamp returns a Carbon object of given timestamp.
 func FromTimestamp(timestamp int64, timezone ...string) Carbon {
 	return carbon.CreateFromTimestamp(timestamp, timezone...)
 }
 
-// FromTimestampMilli return a Carbon object of given millisecond timestamp.
+// FromTimestampMilli returns a Carbon object of given millisecond timestamp.
 func FromTimestampMilli(timestamp int64, timezone ...string) Carbon {
 	return carbon.CreateFromTimestampMilli(timestamp, timezone...)
 }
 
-// FromTimestampMicro return a Carbon object of given microsecond timestamp.
+// FromTimestampMicro returns a Carbon object of given microsecond timestamp.
 func FromTimestampMicro(timestamp int64, timezone ...string) Carbon {
 	return carbon.CreateFromTimestampMicro(timestamp, timezone...)
 }
 
-// FromTimestampNano return a Carbon object of given nanosecond timestamp.
+// FromTimestampNano returns a Carbon object of given nanosecond timestamp.
 func FromTimestampNano(timestamp int64, timezone ...string) Carbon {
 	return carbon.CreateFromTimestampNano(timestamp, timezone...)
 }
 
-// FromDateTime return a Carbon object of given date and time.
+// FromDateTime returns a Carbon object of given date and time.
 func FromDateTime(year int, month int, day int, hour int, minute int, second int, timezone ...string) Carbon {
 	return carbon.CreateFromDateTime(year, month, day, hour, minute, second, timezone...)
 }
 
-// FromDateTimeMilli return a Carbon object of given date and millisecond time.
+// FromDateTimeMilli returns a Carbon object of given date and millisecond time.
 func FromDateTimeMilli(year int, month int, day int, hour int, minute int, second int, millisecond int, timezone ...string) Carbon {
 	return carbon.CreateFromDateTimeMilli(year, month, day, hour, minute, second, millisecond, timezone...)
 }
 
-// FromDateTimeMicro return a Carbon object of given date and microsecond time.
+// FromDateTimeMicro returns a Carbon object of given date and microsecond time.
 func FromDateTimeMicro(year int, month int, day int, hour int, minute int, second int, microsecond int, timezone ...string) Carbon {
 	return carbon.CreateFromDateTimeMicro(year, month, day, hour, minute, second, microsecond, timezone...)
 }
 
-// FromDateTimeNano return a Carbon object of given date and nanosecond time.
+// FromDateTimeNano returns a Carbon object of given date and nanosecond time.
 func FromDateTimeNano(year int, month int, day int, hour int, minute int, second int, nanosecond int, timezone ...string) Carbon {
 	return carbon.CreateFromDateTimeNano(year, month, day, hour, minute, second, nanosecond, timezone...)
 }
 
-// FromDate return a Carbon object of given date.
+// FromDate returns a Carbon object of given date.
 func FromDate(year int, month int, day int, timezone ...string) Carbon {
 	return carbon.CreateFromDate(year, month, day, timezone...)
 }
 
-// FromDateMilli return a Carbon object of given millisecond date.
+// FromDateMilli returns a Carbon object of given millisecond date.
 func FromDateMilli(year int, month int, day int, millisecond int, timezone ...string) Carbon {
 	return carbon.CreateFromDateMilli(year, month, day, millisecond, timezone...)
 }
 
-// FromDateMicro return a Carbon object of given microsecond date.
+// FromDateMicro returns a Carbon object of given microsecond date.
 func FromDateMicro(year int, month int, day int, microsecond int, timezone ...string) Carbon {
 	return carbon.CreateFromDateMicro(year, month, day, microsecond, timezone...)
 }
 
-// FromDateNano return a Carbon object of given nanosecond date.
+// FromDateNano returns a Carbon object of given nanosecond date.
 func FromDateNano(year int, month int, day int, nanosecond int, timezone ...string) Carbon {
 	return carbon.CreateFromDateNano(year, month, day, nanosecond, timezone...)
 }
 
-// FromTime return a Carbon object of given time.
+// FromTime returns a Carbon object of given time.
 func FromTime(hour int, minute int, second int, timezone ...string) Carbon {
 	return carbon.CreateFromTime(hour, minute, second, timezone...)
 }
 
-// FromTimeMilli return a Carbon object of given millisecond time.
+// FromTimeMilli returns a Carbon object of given millisecond time.
 func FromTimeMilli(hour int, minute int, second int, millisecond int, timezone ...string) Carbon {
 	return carbon.CreateFromTimeMilli(hour, minute, second, millisecond, timezone...)
 }
 
-// FromTimeMicro return a Carbon object of given microsecond time.
+// FromTimeMicro returns a Carbon object of given microsecond time.
 func FromTimeMicro(hour int, minute int, second int, microsecond int, timezone ...string) Carbon {
 	return carbon.CreateFromTimeMicro(hour, minute, second, microsecond, timezone...)
 }
 
-// FromTimeNano return a Carbon object of given nanosecond time.
+// FromTimeNano returns a Carbon object of given nanosecond time.
 func FromTimeNano(hour int, minute int, second int, nanosecond int, timezone ...string) Carbon {
 	return carbon.CreateFromTimeNano(hour, minute, second, nanosecond, timezone...)
 }
 
-// FromStdTime return a Carbon object of given time.Time object.
+// FromStdTime returns a Carbon object of given time.Time object.
 func FromStdTime(time stdtime.Time, timezone ...string) Carbon {
 	return carbon.CreateFromStdTime(time, timezone...)
 }
