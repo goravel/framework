ab35803bf7e400943174672de59663549f12db8d
almas-x <almas.cc@icloud.com>
2025-06-23 18:24:14 +0800

    feat: [#716] support artisan route:list command

--- route/service_provider.go
+++ route/service_provider.go
@@ -2,8 +2,10 @@ package route
 
 import (
 	"github.com/goravel/framework/contracts"
+	"github.com/goravel/framework/contracts/console"
 	"github.com/goravel/framework/contracts/foundation"
 	"github.com/goravel/framework/errors"
+	routeconsole "github.com/goravel/framework/route/console"
 )
 
 type ServiceProvider struct {
@@ -21,5 +23,11 @@ func (r *ServiceProvider) Register(app foundation.Application) {
 }
 
 func (r *ServiceProvider) Boot(app foundation.Application) {
+	r.registerCommands(app)
+}
 
+func (r *ServiceProvider) registerCommands(app foundation.Application) {
+	app.MakeArtisan().Register([]console.Command{
+		routeconsole.NewList(app.MakeRoute()),
+	})
 }
