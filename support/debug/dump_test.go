859ef849536b14a331abc3b707665504ee074dd7
almas-x <almas.cc@icloud.com>
2025-07-05 19:00:07 +0800

    chore: fix test

--- support/debug/dump_test.go
+++ support/debug/dump_test.go
@@ -47,7 +47,7 @@ func TestDd(t *testing.T) {
 		DD(struct{ ID int }{ID: 123})
 	})
 	assert.Equal(t, "\x1b[90m<#dump // dump.go:19"+
-		"\x1b[0m\n\x1b[90m#struct { ID int }\x1b[0m \n  "+
+		"\x1b[0m\n\x1b[90m#struct { ID int }\x1b[0m {\n  "+
 		"\x1b[33m+\x1b[0mID => \x1b[38;5;38m123\x1b[0m\n}\n",
 		output)
 }
@@ -70,12 +70,12 @@ func TestDump(t *testing.T) {
 			Dump(data)
 		})
 		assert.Equal(t, "\x1b[90m<#dump // dump.go:25"+
-			"\x1b[0m\n\x1b[90m#debug.info\x1b[0m \n  "+
+			"\x1b[0m\n\x1b[90m#debug.info\x1b[0m {\n  "+
 			"\x1b[33m+\x1b[0mName    => \x1b[33m\"\x1b[0m\x1b[38;5;113mtest"+
 			"\x1b[0m\x1b[33m\"\x1b[0m\n  \x1b[33m-\x1b[0membed   => \x1b[90m#debug.embed"+
-			"\x1b[0m \n    \x1b[33m+\x1b[0mFoo   => \x1b[33m\"\x1b[0m\x1b[38;5;113mfoo"+
+			"\x1b[0m {\n    \x1b[33m+\x1b[0mFoo   => \x1b[33m\"\x1b[0m\x1b[38;5;113mfoo"+
 			"\x1b[0m\x1b[33m\"\x1b[0m\n    \x1b[33m+\x1b[0mBar   => \x1b[90m#struct { Baz string }"+
-			"\x1b[0m \n      \x1b[33m+\x1b[0mBaz => \x1b[33m\"\x1b[0m\x1b[38;5;113mbaz"+
+			"\x1b[0m {\n      \x1b[33m+\x1b[0mBaz => \x1b[33m\"\x1b[0m\x1b[38;5;113mbaz"+
 			"\x1b[0m\x1b[33m\"\x1b[0m\n    }\n  }\n}\n",
 			output)
 	})
@@ -84,14 +84,14 @@ func TestDump(t *testing.T) {
 		output := captureOutput(func() {
 			DumpHTML(data)
 		})
-		assert.Equal(t, "<body style='background-color:black;'><pre style=\"background-color:black; color:white; padding:5px; border-radius: 5px\">\n"+
-			"<span style=\"color:#999\"><#dump // dump.go:30</span>\n<span style=\"color:#999\">#debug.info</span> \n  "+
+		assert.Equal(t, "<div style='background-color:black;'><pre style=\"background-color:black; color:white; padding:5px; border-radius: 5px\">\n"+
+			"<span style=\"color:#999\"><#dump // dump.go:30</span>\n<span style=\"color:#999\">#debug.info</span> {\n  "+
 			"<span style=\"color:#ffb400\">+</span>Name    => <span style=\"color:#ffb400\">\"</span><span style=\"color:#80ff80\">test"+
-			"</span><span style=\"color:#ffb400\">\"</span>\n  <span style=\"color:#ffb400\">-</span>embed   => <span style=\"color:#999\">#debug.embed</span> \n"+
+			"</span><span style=\"color:#ffb400\">\"</span>\n  <span style=\"color:#ffb400\">-</span>embed   => <span style=\"color:#999\">#debug.embed</span> {\n"+
 			"    <span style=\"color:#ffb400\">+</span>Foo   => <span style=\"color:#ffb400\">\"</span><span style=\"color:#80ff80\">foo</span>"+
-			"<span style=\"color:#ffb400\">\"</span>\n    <span style=\"color:#ffb400\">+</span>Bar   => <span style=\"color:#999\">#struct { Baz string }</span> \n"+
+			"<span style=\"color:#ffb400\">\"</span>\n    <span style=\"color:#ffb400\">+</span>Bar   => <span style=\"color:#999\">#struct { Baz string }</span> {\n"+
 			"      <span style=\"color:#ffb400\">+</span>Baz => <span style=\"color:#ffb400\">\"</span><span style=\"color:#80ff80\">baz</span>"+
-			"<span style=\"color:#ffb400\">\"</span>\n    }\n  }\n}\n</pre></body>\n", output)
+			"<span style=\"color:#ffb400\">\"</span>\n    }\n  }\n}\n</pre></div>\n", output)
 	})
 
 	t.Run("dump with JSON", func(t *testing.T) {
@@ -119,9 +119,9 @@ func TestFDump(t *testing.T) {
 	t.Run("dump HTML to writer", func(t *testing.T) {
 		var buf bytes.Buffer
 		FDumpHTML(&buf, data)
-		assert.Equal(t, "<body style='background-color:black;'><pre style=\"background-color:black; color:white; padding:5px; border-radius: 5px\">\n"+
+		assert.Equal(t, "<div style='background-color:black;'><pre style=\"background-color:black; color:white; padding:5px; border-radius: 5px\">\n"+
 			"<span style=\"color:#999\"><#dump // dump_test.go:121</span>\n{\n   <span style=\"color:#d087d0\">key</span> => <span style=\"color:#ffb400\">\"</span>"+
-			"<span style=\"color:#80ff80\">value</span><span style=\"color:#ffb400\">\"</span>\n}\n</pre></body>\n",
+			"<span style=\"color:#80ff80\">value</span><span style=\"color:#ffb400\">\"</span>\n}\n</pre></div>\n",
 			buf.String())
 
 	})
@@ -139,7 +139,7 @@ func TestSDump(t *testing.T) {
 
 	t.Run("dump as string", func(t *testing.T) {
 		output := SDump(data)
-		assert.Equal(t, "\x1b[90m<#dump // dump.go:58\x1b[0m\n[\n  "+
+		assert.Equal(t, "\x1b[90m<#dump // dump_test.go:141\x1b[0m\n[\n  "+
 			"\x1b[38;5;38m0\x1b[0m => \x1b[33m\"\x1b[0m\x1b[38;5;113mone"+
 			"\x1b[0m\x1b[33m\"\x1b[0m\n  \x1b[38;5;38m1\x1b[0m => \x1b[33m\""+
 			"\x1b[0m\x1b[38;5;113mtwo\x1b[0m\x1b[33m\"\x1b[0m\n]\n",
@@ -148,10 +148,10 @@ func TestSDump(t *testing.T) {
 
 	t.Run("dump HTML as string", func(t *testing.T) {
 		output := SDumpHTML(data)
-		assert.Equal(t, "<body style='background-color:black;'><pre style=\"background-color:black; color:white; padding:5px; border-radius: 5px\">\n"+
-			"<span style=\"color:#999\"><#dump // dump.go:63</span>\n[\n  <span style=\"color:#40c0ff\">0</span> => <span style=\"color:#ffb400\">\"</span>"+
+		assert.Equal(t, "<div style='background-color:black;'><pre style=\"background-color:black; color:white; padding:5px; border-radius: 5px\">\n"+
+			"<span style=\"color:#999\"><#dump // dump_test.go:150</span>\n[\n  <span style=\"color:#40c0ff\">0</span> => <span style=\"color:#ffb400\">\"</span>"+
 			"<span style=\"color:#80ff80\">one</span><span style=\"color:#ffb400\">\"</span>\n  <span style=\"color:#40c0ff\">1</span> => "+
-			"<span style=\"color:#ffb400\">\"</span><span style=\"color:#80ff80\">two</span><span style=\"color:#ffb400\">\"</span>\n]\n</pre></body>",
+			"<span style=\"color:#ffb400\">\"</span><span style=\"color:#80ff80\">two</span><span style=\"color:#ffb400\">\"</span>\n]\n</pre></div>",
 			output)
 	})
 
