26cada364be9f906400ad8b3da1f75e71ce1b4ce
almas-x <almas.cc@icloud.com>
2025-08-09 15:53:08 +0800

    chore: optimize package stubs with config file overwrite and remove steps

--- foundation/console/package_make_command_stubs.go
+++ foundation/console/package_make_command_stubs.go
@@ -144,20 +144,28 @@ import (
 	"github.com/goravel/framework/packages"
 	"github.com/goravel/framework/packages/match"
 	"github.com/goravel/framework/packages/modify"
+	"github.com/goravel/framework/support/file"
 	"github.com/goravel/framework/support/path"
 )
 
 func main() {
+	config, err := file.GetPackageContent(packages.GetModulePath(), "setup/config/DummyName.go")
+	if err != nil {
+		panic(err)
+	}
+
 	packages.Setup(os.Args).
 		Install(
 			modify.GoFile(path.Config("app.go")).
 				Find(match.Imports()).Modify(modify.AddImport(packages.GetModulePath())).
 				Find(match.Providers()).Modify(modify.Register("&DummyName.ServiceProvider{}")),
+			modify.File(path.Config("DummyName.go")).Overwrite(config),
 		).
 		Uninstall(
 			modify.GoFile(path.Config("app.go")).
 				Find(match.Providers()).Modify(modify.Unregister("&DummyName.ServiceProvider{}")).
 				Find(match.Imports()).Modify(modify.RemoveImport(packages.GetModulePath())),
+			modify.File(path.Config("DummyName.go")).Remove(),
 		).
 		Execute()
 }
