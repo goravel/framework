2d55ad36532af1482106999abdd94bb37a2d325f
kkumar-gcc <krishkumar9352@gmail.com>
2025-08-23 23:38:41 +0530

    optimize output_writer

--- process/output_writer.go
+++ process/output_writer.go
@@ -1,20 +1,43 @@
 package process
 
-import contractsprocess "github.com/goravel/framework/contracts/process"
+import (
+	"bufio"
+	"bytes"
+
+	contractsprocess "github.com/goravel/framework/contracts/process"
+)
 
 func NewOutputWriter(typ contractsprocess.OutputType, handler contractsprocess.OnOutputFunc) *OutputWriter {
 	return &OutputWriter{
-		handler: handler,
 		typ:     typ,
+		handler: handler,
+		buffer:  &bytes.Buffer{},
 	}
 }
 
 type OutputWriter struct {
 	typ     contractsprocess.OutputType
 	handler contractsprocess.OnOutputFunc
+	buffer  *bytes.Buffer
 }
 
 func (w *OutputWriter) Write(p []byte) (n int, err error) {
-	w.handler(w.typ, p)
-	return len(p), nil
+	n, err = w.buffer.Write(p)
+	if err != nil {
+		return n, err
+	}
+
+	scanner := bufio.NewScanner(w.buffer)
+	for scanner.Scan() {
+		line := scanner.Bytes()
+		copied := make([]byte, len(line))
+		copy(copied, line)
+		w.handler(w.typ, copied)
+	}
+
+	rest := w.buffer.Bytes()
+	w.buffer.Reset()
+	w.buffer.Write(rest)
+
+	return n, err
 }
