7e25ea1d9ae87582d4fef0ed0d8488546e125420
ALMAS <almas.cc@icloud.com>
2025-08-26 10:22:25 +0800

    feat: refactor build command to support interactive and custom OS/arch selection (#1175)

--- console/console/build_command.go
+++ console/console/build_command.go
@@ -3,11 +3,12 @@ package console
 import (
 	"fmt"
 	"os/exec"
-	"slices"
+	"runtime"
 
 	"github.com/goravel/framework/contracts/config"
 	"github.com/goravel/framework/contracts/console"
 	"github.com/goravel/framework/contracts/console/command"
+	supportconsole "github.com/goravel/framework/support/console"
 )
 
 type BuildCommand struct {
@@ -35,21 +36,26 @@ func (r *BuildCommand) Extend() command.Extend {
 	return command.Extend{
 		Flags: []command.Flag{
 			&command.StringFlag{
+				Name:    "arch",
+				Aliases: []string{"a"},
+				Usage:   "Target architecture",
+				Value:   "amd64",
+			},
+			&command.StringFlag{
 				Name:    "os",
 				Aliases: []string{"o"},
-				Value:   "",
 				Usage:   "Target os",
 			},
 			&command.BoolFlag{
-				Name:    "static",
-				Aliases: []string{"s"},
-				Value:   false,
-				Usage:   "Static compilation",
+				Name:               "static",
+				Aliases:            []string{"s"},
+				Value:              false,
+				Usage:              "Static compilation",
+				DisableDefaultText: true,
 			},
 			&command.StringFlag{
 				Name:    "name",
 				Aliases: []string{"n"},
-				Value:   "",
 				Usage:   "Output binary name",
 			},
 		},
@@ -72,53 +78,41 @@ func (r *BuildCommand) Handle(ctx console.Context) error {
 
 	os := ctx.Option("os")
 	if os == "" {
-		os, err = ctx.Choice("Select target os", []console.Choice{
+		if os, err = ctx.Choice("Select target os", []console.Choice{
 			{Key: "Linux", Value: "linux"},
 			{Key: "Windows", Value: "windows"},
 			{Key: "Darwin", Value: "darwin"},
-		})
-		if err != nil {
+		}, console.ChoiceOption{Default: runtime.GOOS}); err != nil {
 			ctx.Error(fmt.Sprintf("Select target os error: %v", err))
 			return nil
 		}
 	}
 
-	validOs := []string{"linux", "windows", "darwin"}
-	if !slices.Contains(validOs, os) {
-		ctx.Error(fmt.Sprintf("Invalid os '%s' specified. Allowed values are: %v", os, validOs))
+	if err = supportconsole.ExecuteCommand(ctx, generateCommand(ctx.Option("name"), os, ctx.Option("arch"), ctx.OptionBool("static")), "Building..."); err != nil {
+		ctx.Error(err.Error())
 		return nil
 	}
 
-	if err := ctx.Spinner("Building...", console.SpinnerOption{
-		Action: func() error {
-			return r.build(os, generateCommand(ctx.Option("name"), ctx.OptionBool("static")))
-		},
-	}); err != nil {
-		ctx.Error(fmt.Sprintf("Build error: %v", err))
-	} else {
-		ctx.Info("Built successfully.")
-	}
+	ctx.Info("Built successfully.")
 
 	return nil
 }
 
-func (r *BuildCommand) build(system string, command []string) error {
-	cmd := exec.Command(command[0], command[1:]...)
-	cmd.Env = append(cmd.Environ(), "CGO_ENABLED=0", "GOARCH=amd64", "GOOS="+system)
-	_, err := cmd.Output()
-	return err
-}
-
-func generateCommand(name string, static bool) []string {
-	commands := []string{"go", "build"}
+func generateCommand(name, os, arch string, static bool) *exec.Cmd {
+	args := []string{"build"}
 
 	if static {
-		commands = append(commands, "-ldflags", "-extldflags -static")
+		args = append(args, "-ldflags", "-extldflags -static")
 	}
 
 	if name != "" {
-		commands = append(commands, "-o", name)
+		args = append(args, "-o", name)
 	}
 
-	return append(commands, ".")
+	args = append(args, ".")
+
+	cmd := exec.Command("go", args...)
+	cmd.Env = append(cmd.Environ(), "CGO_ENABLED=0", "GOARCH="+arch, "GOOS="+os)
+
+	return cmd
 }
