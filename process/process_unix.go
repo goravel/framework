b9854bc69983623b241c6ee16338bb710d58819b
kkumar-gcc <krishkumar9352@gmail.com>
2025-08-31 12:58:17 +0530

    move all process related function for process_{platform}

--- process/process_unix.go
+++ process/process_unix.go
@@ -3,11 +3,60 @@
 package process
 
 import (
+	"errors"
+	"os"
 	"os/exec"
-	"syscall"
+	"time"
+
+	"golang.org/x/sys/unix"
 )
 
-// setSysProcAttr sets POSIX-specific process attributes.
 func setSysProcAttr(cmd *exec.Cmd) {
-	cmd.SysProcAttr = &syscall.SysProcAttr{Setpgid: true}
+	cmd.SysProcAttr = &unix.SysProcAttr{Setpgid: true}
+}
+
+func running(p *os.Process) bool {
+	if p == nil {
+		return false
+	}
+
+	err := p.Signal(unix.Signal(0))
+	return err == nil
+}
+
+func kill(p *os.Process) error {
+	return signal(p, unix.SIGKILL)
+}
+
+func signal(p *os.Process, sig os.Signal) error {
+	if p == nil {
+		return errors.New("process not started")
+	}
+
+	return p.Signal(sig)
+}
+
+func stop(p *os.Process, done <-chan struct{}, timeout time.Duration, sig ...os.Signal) error {
+	if !running(p) {
+		return nil
+	}
+
+	var signalToSend os.Signal = unix.SIGTERM
+	if len(sig) > 0 {
+		signalToSend = sig[0]
+	}
+
+	if err := signal(p, signalToSend); err != nil {
+		if errors.Is(err, os.ErrProcessDone) {
+			return nil
+		}
+		return err
+	}
+
+	select {
+	case <-done:
+		return nil
+	case <-time.After(timeout):
+		return signal(p, unix.SIGKILL)
+	}
 }
--- process/process_windows.go
+++ process/process_windows.go
@@ -2,7 +2,58 @@
 
 package process
 
-import "os/exec"
+import (
+	"errors"
+	"os"
+	"os/exec"
+	"time"
+
+	"golang.org/x/sys/windows"
+)
+
+// STILL_ACTIVE is a Win32 constant that indicates a process is still running.
+// It is not exported by the Go standard library, so we define it here.
+const STILL_ACTIVE = 259
 
 // setSysProcAttr is a no-op on Windows; Setpgid isn't available.
 func setSysProcAttr(cmd *exec.Cmd) {}
+
+func running(p *os.Process) bool {
+	if p == nil {
+		return false
+	}
+
+	h, err := windows.OpenProcess(windows.PROCESS_QUERY_LIMITED_INFORMATION, false, uint32(p.Pid))
+	if err != nil {
+		// If we cannot open the process (access denied or not found), assume not running.
+		return false
+	}
+	defer windows.CloseHandle(h)
+
+	var code uint32
+	if err := windows.GetExitCodeProcess(h, &code); err != nil {
+		return false
+	}
+	return code == STILL_ACTIVE
+}
+
+func kill(p *os.Process) error {
+	if p == nil {
+		return errors.New("process not started")
+	}
+	return p.Kill()
+}
+
+func signal(p *os.Process, sig os.Signal) error {
+	if p == nil {
+		return errors.New("process not started")
+	}
+	return p.Signal(sig)
+}
+
+func stop(p *os.Process, _ <-chan struct{}, _ time.Duration, _ ...os.Signal) error {
+	if !running(p) {
+		return nil
+	}
+	return kill(p)
+}
