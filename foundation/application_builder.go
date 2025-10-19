74dc501f66215cf7ed3cffba496b23e34743480a
Wenbo Han <hwbrzzl@gmail.com>
2025-10-19 16:40:45 +0800

    feat: [SUB-786] Add WithEvents function (#1234)
    * feat: [SUB-786] Add WithEvents function
    
    * optimize
    
    * fix ci
    
    ---------
    
    Co-authored-by: Bowen <hwbrzzl@github.com>

--- foundation/application_builder.go
+++ foundation/application_builder.go
@@ -1,14 +1,19 @@
 package foundation
 
-import "github.com/goravel/framework/contracts/foundation"
+import (
+	"github.com/goravel/framework/contracts/event"
+	"github.com/goravel/framework/contracts/foundation"
+	"github.com/goravel/framework/support/color"
+)
 
 func Configure() foundation.ApplicationBuilder {
 	return NewApplicationBuilder(App)
 }
 
 type ApplicationBuilder struct {
-	app    foundation.Application
-	config func()
+	app              foundation.Application
+	config           func()
+	eventToListeners map[event.Event][]event.Listener
 }
 
 func NewApplicationBuilder(app foundation.Application) *ApplicationBuilder {
@@ -24,6 +29,15 @@ func (r *ApplicationBuilder) Create() foundation.Application {
 		r.config()
 	}
 
+	if len(r.eventToListeners) > 0 {
+		event := r.app.MakeEvent()
+		if event == nil {
+			color.Errorln("Event facade not found, please install it first: ./artisan package:install Event")
+		} else {
+			event.Register(r.eventToListeners)
+		}
+	}
+
 	return r.app
 }
 
@@ -33,6 +47,12 @@ func (r *ApplicationBuilder) Run() {
 
 func (r *ApplicationBuilder) WithConfig(fn func()) foundation.ApplicationBuilder {
 	r.config = fn
+
+	return r
+}
+
+func (r *ApplicationBuilder) WithEvents(eventToListeners map[event.Event][]event.Listener) foundation.ApplicationBuilder {
+	r.eventToListeners = eventToListeners
 
 	return r
 }
