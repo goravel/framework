ee48c515fe949b7b6dd9042d424b594f409ebd0a
Bowen <hwbrzzl@github.com>
2025-10-19 08:40:46 +0800

    fix ci

--- foundation/application_builder_test.go
+++ foundation/application_builder_test.go
@@ -28,22 +28,73 @@ func (s *ApplicationBuilderTestSuite) SetupTest() {
 }
 
 func (s *ApplicationBuilderTestSuite) TestCreate() {
-	s.mockApp.EXPECT().Boot().Return().Once()
-	mockEvent := mocksevent.NewInstance(s.T())
-	mockEvent.EXPECT().Register(map[event.Event][]event.Listener{}).Return().Once()
-	s.mockApp.EXPECT().MakeEvent().Return(mockEvent).Once()
+	s.Run("Without any With functions", func() {
+		s.mockApp.EXPECT().Boot().Return().Once()
 
-	calledConfig := false
+		app := s.builder.Create()
 
-	app := s.builder.
-		WithConfig(func() {
-			calledConfig = true
-		}).
-		WithEvents(map[event.Event][]event.Listener{}).
-		Create()
+		s.NotNil(app)
+	})
 
-	s.NotNil(app)
-	s.True(calledConfig)
+	s.Run("WithConfig", func() {
+		calledConfig := false
+
+		s.mockApp.EXPECT().Boot().Return().Once()
+
+		app := s.builder.
+			WithConfig(func() {
+				calledConfig = true
+			}).
+			Create()
+
+		s.NotNil(app)
+		s.True(calledConfig)
+	})
+
+	s.Run("WithEvents but Event facade is nil", func() {
+		s.mockApp.EXPECT().Boot().Return().Once()
+		s.mockApp.EXPECT().MakeEvent().Return(nil).Once()
+
+		app := s.builder.
+			WithEvents(map[event.Event][]event.Listener{
+				mocksevent.NewEvent(s.T()): {mocksevent.NewListener(s.T())},
+			}).
+			Create()
+
+		s.NotNil(app)
+	})
+
+	s.Run("WithEvents but Events is empty", func() {
+		s.mockApp.EXPECT().Boot().Return().Once()
+
+		app := s.builder.
+			WithEvents(map[event.Event][]event.Listener{}).
+			Create()
+
+		s.NotNil(app)
+	})
+
+	s.Run("WithEvents", func() {
+		s.mockApp.EXPECT().Boot().Return().Once()
+		mockEvent := mocksevent.NewEvent(s.T())
+		mockListener := mocksevent.NewListener(s.T())
+		mockEventFacade := mocksevent.NewInstance(s.T())
+		mockEventFacade.EXPECT().
+			Register(map[event.Event][]event.Listener{
+				mockEvent: {mockListener},
+			}).
+			Return().
+			Once()
+		s.mockApp.EXPECT().MakeEvent().Return(mockEventFacade).Once()
+
+		app := s.builder.
+			WithEvents(map[event.Event][]event.Listener{
+				mockEvent: {mockListener},
+			}).
+			Create()
+
+		s.NotNil(app)
+	})
 }
 
 func (s *ApplicationBuilderTestSuite) TestRun() {
