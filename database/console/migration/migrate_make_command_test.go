95dd6d811674559dda376e9fc5b07386a3cd0529
kkumar-gcc <krishkumar9352@gmail.com>
2025-10-27 00:56:00 +0530

    fix lint

--- database/console/migration/migrate_make_command_test.go
+++ database/console/migration/migrate_make_command_test.go
@@ -36,7 +36,7 @@ func TestMigrateMakeCommand(t *testing.T) {
 			setup: func() {
 				mockContext.EXPECT().Argument(0).Return("").Once()
 				mockContext.EXPECT().Ask("Enter the migration name", mock.Anything).Return("create_users_table", nil).Once()
-				mockMigrator.EXPECT().Create("create_users_table").Return("", nil).Once()
+				mockMigrator.EXPECT().Create("create_users_table", "").Return("", nil).Once()
 				mockContext.EXPECT().Success("Created Migration: create_users_table").Once()
 				mockApp.EXPECT().DatabasePath("kernel.go").Return("database/kernel.go").Once()
 				mockContext.EXPECT().Warning(mock.MatchedBy(func(msg string) bool {
@@ -48,7 +48,7 @@ func TestMigrateMakeCommand(t *testing.T) {
 			name: "Happy path - name is not empty",
 			setup: func() {
 				mockContext.EXPECT().Argument(0).Return("create_users_table").Once()
-				mockMigrator.EXPECT().Create("create_users_table").Return("", nil).Once()
+				mockMigrator.EXPECT().Create("create_users_table", "").Return("", nil).Once()
 				mockContext.EXPECT().Success("Created Migration: create_users_table").Once()
 				mockApp.EXPECT().DatabasePath("kernel.go").Return("database/kernel.go").Once()
 				mockContext.EXPECT().Warning(mock.MatchedBy(func(msg string) bool {
@@ -68,7 +68,7 @@ func TestMigrateMakeCommand(t *testing.T) {
 			name: "Sad path - failed to create",
 			setup: func() {
 				mockContext.EXPECT().Argument(0).Return("create_users_table").Once()
-				mockMigrator.EXPECT().Create("create_users_table").Return("", assert.AnError).Once()
+				mockMigrator.EXPECT().Create("create_users_table", "").Return("", assert.AnError).Once()
 				mockContext.EXPECT().Error(errors.MigrationCreateFailed.Args(assert.AnError).Error()).Once()
 			},
 		},
@@ -76,7 +76,7 @@ func TestMigrateMakeCommand(t *testing.T) {
 			name: "Register success",
 			setup: func() {
 				mockContext.EXPECT().Argument(0).Return("create_users_table").Once()
-				mockMigrator.EXPECT().Create("create_users_table").Return("20240915060148_create_users_table", nil).Once()
+				mockMigrator.EXPECT().Create("create_users_table", "").Return("20240915060148_create_users_table", nil).Once()
 				mockContext.EXPECT().Success("Created Migration: create_users_table").Once()
 				mockApp.EXPECT().DatabasePath("kernel.go").Return("database/kernel.go").Once()
 				mockContext.EXPECT().Success("Migration registered successfully").Once()
