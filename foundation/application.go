ade9a75212816823b17e492538b3f6bb6fe93d02
Adam Jimerson <vendion@gmail.com>
2025-11-16 09:53:29 -0500

    feat: Add ModelPath to foundation.Application
    Add a new `ModelPath` method to `foundation.Application` to assist with
    package development that defines additional models.
    
    closes: https://github.com/goravel/goravel/issues/814
    Signed-off-by: Adam Jimerson <vendion@gmail.com>

--- foundation/application.go
+++ foundation/application.go
@@ -218,6 +218,11 @@ func (r *Application) ConfigPath(path ...string) string {
 	return internals.AbsPath(path...)
 }
 
+func (r *Application) ModelPath(path ...string) string {
+	path = append([]string{support.RelativePath, "app", "models"}, path...)
+	return internals.AbsPath(path...)
+}
+
 func (r *Application) DatabasePath(path ...string) string {
 	path = append([]string{support.RelativePath, "database"}, path...)
 	return internals.AbsPath(path...)
