3d4bdc401109236543fd1d38f34e247b7349fe55
Bowen <hwbrzzl@gmail.com>
2025-12-05 22:37:43 +0800

    feat: [#822] Remove unnecessary code

--- schedule/setup/setup.go
+++ schedule/setup/setup.go
@@ -5,23 +5,17 @@ import (
 
 	"github.com/goravel/framework/packages"
 	"github.com/goravel/framework/packages/modify"
-	"github.com/goravel/framework/support/file"
 	"github.com/goravel/framework/support/path"
-	"github.com/goravel/framework/support/stubs"
 )
 
 func main() {
 	scheduleFacade := "Schedule"
-	kernelPath := path.App("console", "kernel.go")
 	scheduleFacadePath := path.Facades("schedule.go")
 	scheduleServiceProvider := "&schedule.ServiceProvider{}"
 	modulePath := packages.GetModulePath()
 
 	packages.Setup(os.Args).
 		Install(
-			// Create the console kernel file if it does not exist.
-			modify.WhenFileNotExists(kernelPath, modify.File(kernelPath).Overwrite(stubs.ConsoleKernel())),
-
 			// Create the schedule facade file.
 			modify.WhenFacade(scheduleFacade, modify.File(scheduleFacadePath).Overwrite(Stubs{}.ScheduleFacade())),
 
@@ -32,22 +26,10 @@ func main() {
 			modify.WhenNoFacades([]string{scheduleFacade},
 				// Remove the schedule service provider from the providers array in bootstrap/providers.go
 				modify.RemoveProviderApply(modulePath, scheduleServiceProvider),
-
-				// Remove the console kernel file if it was not modified.
-				modify.When(isKernelNotModified, modify.File(kernelPath).Remove()),
 			),
 
 			// Remove the schedule facade file.
 			modify.WhenFacade(scheduleFacade, modify.File(scheduleFacadePath).Remove()),
 		).
 		Execute()
-}
-
-func isKernelNotModified(_ map[string]any) bool {
-	content, err := file.GetContent(path.App("console", "kernel.go"))
-	if err != nil {
-		return false
-	}
-
-	return content == stubs.ConsoleKernel()
 }
