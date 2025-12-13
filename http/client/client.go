bc11828924a78adf18c4076f96cc86aae338411c
kkumar-gcc <krishkumar9352@gmail.com>
2025-12-13 20:05:37 +0530

    optimise client

--- http/client/client.go
+++ http/client/client.go
@@ -8,21 +8,22 @@ import (
 )
 
 var (
-	httpClient     *http.Client
+	httpTransport  *http.Transport
 	httpClientOnce sync.Once
 )
 
 func getHttpClient(config *client.Config) *http.Client {
 	httpClientOnce.Do(func() {
-		httpClient = &http.Client{
-			Timeout: config.Timeout,
-			Transport: &http.Transport{
-				MaxIdleConns:        config.MaxIdleConns,
-				MaxIdleConnsPerHost: config.MaxIdleConnsPerHost,
-				MaxConnsPerHost:     config.MaxConnsPerHost,
-				IdleConnTimeout:     config.IdleConnTimeout,
-			},
+		httpTransport = &http.Transport{
+			MaxIdleConns:        config.MaxIdleConns,
+			MaxIdleConnsPerHost: config.MaxIdleConnsPerHost,
+			MaxConnsPerHost:     config.MaxConnsPerHost,
+			IdleConnTimeout:     config.IdleConnTimeout,
 		}
 	})
-	return httpClient
+
+	return &http.Client{
+		Timeout:   config.Timeout,
+		Transport: httpTransport,
+	}
 }
