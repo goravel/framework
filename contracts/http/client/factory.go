84cd6cf96c7cb64210aa6f3d6c2496c49f5fa780
kkumar-gcc <krishkumar9352@gmail.com>
2025-12-28 15:33:10 +0530

    Optimise factory interface

--- contracts/http/client/factory.go
+++ contracts/http/client/factory.go
@@ -1,7 +1,22 @@
 package client
 
 type Factory interface {
-	// Client returns a named HTTP client.
+	// Client returns a configured HTTP client by name.
+	//
 	// If no name is provided, the default client is returned.
+	// If the requested client is not configured, this method panics.
+	//
+	// This method is intended for advanced usage where the caller
+	// wants to reuse a client instance or create multiple requests
+	// from the same client.
 	Client(name ...string) Client
+
+	// Request returns a new HTTP request builder for the given client.
+	//
+	// This is a convenience method equivalent to:
+	//   Http().Client(name).NewRequest()
+	//
+	// If no name is provided, the default client is used.
+	// If the requested client is not configured, this method panics.
+	Request(name ...string) Request
 }
