d944fe826c8a6dddb03a14bb258467771bd909a5
Bowen <hwbrzzl@gmail.com>
2025-12-29 11:31:36 +0800

    fix: [#840] DB cache is not reset when fresh orm

--- database/orm/orm.go
+++ database/orm/orm.go
@@ -12,6 +12,7 @@ import (
 	"github.com/goravel/framework/contracts/database"
 	contractsorm "github.com/goravel/framework/contracts/database/orm"
 	"github.com/goravel/framework/contracts/log"
+	"github.com/goravel/framework/database/driver"
 	"github.com/goravel/framework/database/factory"
 	"github.com/goravel/framework/database/gorm"
 )
@@ -140,8 +141,11 @@ func (r *Orm) SetQuery(query contractsorm.Query) {
 	r.query = query
 }
 
+// TODO: The fresh logic needs to be optimized, it's a bit unclear now.
+// https://github.com/goravel/goravel/issues/848
 func (r *Orm) Fresh() {
 	r.fresh(binding.Orm)
+	driver.ResetConnections()
 }
 
 func (r *Orm) Transaction(txFunc func(tx contractsorm.Query) error) (err error) {
