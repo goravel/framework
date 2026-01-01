3ee04f3ff176b43b9342e72788ce84dd6ede03db
kkumar-gcc <krishkumar9352@gmail.com>
2026-01-01 17:04:44 +0530

    add default client configs

--- http/setup/stubs.go
+++ http/setup/stubs.go
@@ -85,12 +85,12 @@ func init() {
         // Map unique names (e.g., "default", "downstream") to their configurations.
 		"clients": map[string]any{
 			"default": map[string]any{
-				"base_url":                config.GetString("HTTP_CLIENT_BASE_URL"),
-				"timeout":                 config.GetDuration("HTTP_CLIENT_TIMEOUT"),
-				"max_idle_conns":          config.GetInt("HTTP_CLIENT_MAX_IDLE_CONNS"),
-				"max_idle_conns_per_host": config.GetInt("HTTP_CLIENT_MAX_IDLE_CONNS_PER_HOST"),
-				"max_conns_per_host":      config.GetInt("HTTP_CLIENT_MAX_CONN_PER_HOST"),
-				"idle_conn_timeout":       config.GetDuration("HTTP_CLIENT_IDLE_CONN_TIMEOUT"),
+				"base_url":                config.Env("HTTP_CLIENT_BASE_URL", ""),
+				"timeout":                 config.Env("HTTP_CLIENT_TIMEOUT", "30s"),
+				"max_idle_conns":          config.Env("HTTP_CLIENT_MAX_IDLE_CONNS", 100),
+				"max_idle_conns_per_host": config.Env("HTTP_CLIENT_MAX_IDLE_CONNS_PER_HOST", 2),
+				"max_conns_per_host":      config.Env("HTTP_CLIENT_MAX_CONN_PER_HOST", 0),
+				"idle_conn_timeout":       config.Env("HTTP_CLIENT_IDLE_CONN_TIMEOUT", "90s"),
 			},
 		},
 	})
