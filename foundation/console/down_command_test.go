eb6fd38c53dca92968db09d371b35a066d876c1b
Mohan Raj <praem1990@gmail.com>
2026-01-05 06:22:44 +0530

    Fix down_command_test

--- foundation/console/down_command_test.go
+++ foundation/console/down_command_test.go
@@ -1,7 +1,7 @@
 package console
 
 import (
-	"os"
+	"encoding/json"
 	"path/filepath"
 	"testing"
 
@@ -42,7 +42,7 @@ func (s *DownCommandTestSuite) TestExtend() {
 	cmd := NewDownCommand(mocksfoundation.NewApplication(s.T()))
 	got := cmd.Extend()
 
-	s.Equal(1, len(got.Flags))
+	s.Equal(6, len(got.Flags))
 }
 
 func (s *DownCommandTestSuite) TestHandle() {
@@ -55,6 +55,11 @@ func (s *DownCommandTestSuite) TestHandle() {
 
 	mockContext := mocksconsole.NewContext(s.T())
 	flag := cmd.Extend().Flags[0].(*command.StringFlag)
+	mockContext.EXPECT().OptionInt("status").Return(503)
+	mockContext.EXPECT().Option("render").Return("")
+	mockContext.EXPECT().Option("redirect").Return("")
+	mockContext.EXPECT().Option("secret").Return("")
+	mockContext.EXPECT().OptionBool("with-secret").Return(false)
 	mockContext.EXPECT().Option("reason").Return(flag.Value)
 	mockContext.EXPECT().Info("The application is in maintenance mode now")
 
@@ -66,7 +71,13 @@ func (s *DownCommandTestSuite) TestHandle() {
 	content, err := file.GetContent(tmpfile)
 
 	assert.Nil(s.T(), err)
-	assert.Equal(s.T(), flag.Value, content)
+
+	var maintenanceOptions *MaintenanceOptions
+	err = json.Unmarshal([]byte(content), &maintenanceOptions)
+	assert.Nil(s.T(), err)
+
+	assert.Equal(s.T(), flag.Value, maintenanceOptions.Reason)
+	assert.Equal(s.T(), 503, maintenanceOptions.Status)
 }
 
 func (s *DownCommandTestSuite) TestHandleWithReason() {
@@ -76,6 +87,11 @@ func (s *DownCommandTestSuite) TestHandleWithReason() {
 	app.EXPECT().StoragePath("framework/maintenance").Return(tmpfile)
 
 	mockContext := mocksconsole.NewContext(s.T())
+	mockContext.EXPECT().OptionInt("status").Return(505)
+	mockContext.EXPECT().Option("render").Return("")
+	mockContext.EXPECT().Option("redirect").Return("")
+	mockContext.EXPECT().Option("secret").Return("")
+	mockContext.EXPECT().OptionBool("with-secret").Return(false)
 	mockContext.EXPECT().Option("reason").Return("Under maintenance")
 	mockContext.EXPECT().Info("The application is in maintenance mode now")
 
@@ -88,25 +104,10 @@ func (s *DownCommandTestSuite) TestHandleWithReason() {
 	content, err := file.GetContent(tmpfile)
 
 	assert.Nil(s.T(), err)
-	assert.Equal(s.T(), "Under maintenance", content)
-}
-
-func (s *DownCommandTestSuite) TestHandleWhenDownAlready() {
-	app := mocksfoundation.NewApplication(s.T())
-	tmpfile := filepath.Join(s.T().TempDir(), "/down_already")
-
-	fd, err := os.Create(tmpfile)
+	var maintenanceOptions *MaintenanceOptions
+	err = json.Unmarshal([]byte(content), &maintenanceOptions)
 	assert.Nil(s.T(), err)
 
-	err = fd.Close()
-	assert.Nil(s.T(), err)
-
-	app.EXPECT().StoragePath("framework/maintenance").Return(tmpfile)
-
-	mockContext := mocksconsole.NewContext(s.T())
-	mockContext.EXPECT().Error("The application is in maintenance mode already!")
-
-	cmd := NewDownCommand(app)
-	err = cmd.Handle(mockContext)
-	assert.Nil(s.T(), err)
+	assert.Equal(s.T(), "Under maintenance", maintenanceOptions.Reason)
+	assert.Equal(s.T(), 505, maintenanceOptions.Status)
 }
