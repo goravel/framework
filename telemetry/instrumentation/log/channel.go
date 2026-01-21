777ce250e2a25782b09ee3bdaf1bdb29ee7cc903
kkumar-gcc <krishkumar9352@gmail.com>
2026-01-22 01:05:40 +0530

    optimise

--- telemetry/instrumentation/log/channel.go
+++ telemetry/instrumentation/log/channel.go
@@ -27,7 +27,7 @@ func NewLazyTelemetryChannel(config contractsconfig.Config, resolver contractste
 }
 
 func (r *TelemetryChannel) Handle(channelPath string) (contractslog.Handler, error) {
-	if !r.config.GetBool("telemetry.instrumentation.log", true) {
+	if r.config == nil || !r.config.GetBool("telemetry.instrumentation.log", true) {
 		return &handler{enabled: false}, nil
 	}
 
