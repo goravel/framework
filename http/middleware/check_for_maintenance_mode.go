59a1ca87f95affd873870fde8b1e8e0db46ead05
Mohan Raj <praem1990@gmail.com>
2026-02-14 13:38:47 +0530

    Fix and Write more unit tests for check_for_maintenance_mode middleware

--- http/middleware/check_for_maintenance_mode.go
+++ http/middleware/check_for_maintenance_mode.go
@@ -2,24 +2,22 @@ package middleware
 
 import (
 	"encoding/json"
-	"os"
 
 	"github.com/goravel/framework/contracts/http"
 	"github.com/goravel/framework/facades"
 	"github.com/goravel/framework/foundation/console"
-	"github.com/goravel/framework/support/file"
-	"github.com/goravel/framework/support/path"
 )
 
 func CheckForMaintenanceMode() http.Middleware {
 	return func(ctx http.Context) {
-		filepath := path.Storage("framework/maintenance")
-		if !file.Exists(filepath) {
+		storage := facades.Storage()
+		filepath := "framework/maintenance.json"
+		if !storage.Exists(filepath) {
 			ctx.Request().Next()
 			return
 		}
 
-		content, err := os.ReadFile(filepath)
+		content, err := storage.GetBytes(filepath)
 
 		if err != nil {
 			if err = ctx.Response().String(http.StatusServiceUnavailable, err.Error()).Abort(); err != nil {
@@ -60,8 +58,8 @@ func CheckForMaintenanceMode() http.Middleware {
 
 		if maintenanceOptions.Render != "" {
 			ctx.Request().Abort(maintenanceOptions.Status)
-			if err = ctx.Response().View().Make(maintenanceOptions.Render, map[string]string{}).Render(); err != nil {
-				return
+			if err = ctx.Response().View().Make(maintenanceOptions.Render).Render(); err != nil {
+				panic(err)
 			}
 			return
 		}
--- http/middleware/check_for_maintenance_mode_test.go
+++ http/middleware/check_for_maintenance_mode_test.go
@@ -1,42 +1,145 @@
 package middleware
 
 import (
-	nethttp "net/http"
-	"net/http/httptest"
+	"errors"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
+	"github.com/stretchr/testify/suite"
 
-	"github.com/goravel/framework/contracts/http"
-	"github.com/goravel/framework/support/file"
-	"github.com/goravel/framework/support/path"
+	contractshttp "github.com/goravel/framework/contracts/http"
+	"github.com/goravel/framework/http"
+	"github.com/goravel/framework/testing/mock"
+
+	mocksfilesystem "github.com/goravel/framework/mocks/filesystem"
+	mocksfoundation "github.com/goravel/framework/mocks/foundation"
+	mockshash "github.com/goravel/framework/mocks/hash"
+	mockshttp "github.com/goravel/framework/mocks/http"
 )
 
-func testHttpCheckForMaintenanceMiddleware(next nethttp.Handler) nethttp.Handler {
-	return nethttp.HandlerFunc(func(w nethttp.ResponseWriter, r *nethttp.Request) {
-		CheckForMaintenanceMode()(NewTestContext(r.Context(), next, w, r))
+type MaintenanceTestSuite struct {
+	suite.Suite
+	mockApp               *mocksfoundation.Application
+	mockCtx               *mockshttp.Context
+	mockFile              *mocksfilesystem.File
+	mockHash              *mockshash.Hash
+	mockStorage           *mocksfilesystem.Storage
+	mockRequest           *mockshttp.ContextRequest
+	mockResponse          *mockshttp.ContextResponse
+	mockAbortableResponse *mockshttp.AbortableResponse
+}
+
+func TestMaintenanceModeSuite(t *testing.T) {
+	suite.Run(t, new(MaintenanceTestSuite))
+}
+
+func (s *MaintenanceTestSuite) SetupTest() {
+	mockFactory := mock.Factory()
+	s.mockApp = mocksfoundation.NewApplication(s.T())
+	s.mockCtx = mockshttp.NewContext(s.T())
+	s.mockFile = mocksfilesystem.NewFile(s.T())
+	s.mockHash = mockFactory.Hash()
+	s.mockRequest = mockshttp.NewContextRequest(s.T())
+	s.mockResponse = mockshttp.NewContextResponse(s.T())
+	s.mockAbortableResponse = mockshttp.NewAbortableResponse(s.T())
+	s.mockStorage = mockFactory.Storage()
+	http.App = s.mockApp
+}
+
+func (s *MaintenanceTestSuite) TestMaintenaneMode_NotUnderMaintenance() {
+	s.mockCtx.EXPECT().Request().Return(s.mockRequest).Once()
+	s.mockRequest.EXPECT().Next().Once()
+	s.mockStorage.EXPECT().Exists("framework/maintenance.json").Return(false).Once()
+
+	middleware := CheckForMaintenanceMode()
+	middleware(s.mockCtx)
+}
+
+func (s *MaintenanceTestSuite) TestMaintenaneMode_StorageFilePermissionIssue() {
+	err := errors.New("permission denied")
+	abortableResponse := mockshttp.NewAbortableResponse(s.T())
+	abortableResponse.EXPECT().Abort().Return(err).Once()
+
+	s.mockCtx.EXPECT().Response().Return(s.mockResponse).Once()
+	s.mockStorage.EXPECT().Exists("framework/maintenance.json").Return(true).Once()
+	s.mockStorage.EXPECT().GetBytes("framework/maintenance.json").Return(nil, err).Once()
+	s.mockResponse.EXPECT().String(contractshttp.StatusServiceUnavailable, err.Error()).Return(abortableResponse).Once()
+
+	middleware := CheckForMaintenanceMode()
+	assert.PanicsWithError(s.T(), err.Error(), func() {
+		middleware(s.mockCtx)
 	})
 }
 
-func TestMaintenanceMode(t *testing.T) {
-	server := httptest.NewServer(testHttpCheckForMaintenanceMiddleware(nethttp.HandlerFunc(func(w nethttp.ResponseWriter, r *nethttp.Request) {
-	})))
-	defer server.Close()
+func (s *MaintenanceTestSuite) TestMaintenaneMode_StorageFileInvalidJSON() {
+	err := errors.New("invalid character 'i' looking for beginning of value")
+	s.mockAbortableResponse.EXPECT().Abort().Return(err).Once()
 
-	client := &nethttp.Client{}
+	s.mockCtx.EXPECT().Response().Return(s.mockResponse).Once()
+	s.mockStorage.EXPECT().Exists("framework/maintenance.json").Return(true).Once()
+	s.mockStorage.EXPECT().GetBytes("framework/maintenance.json").Return([]byte("invalid json"), nil).Once()
+	s.mockResponse.EXPECT().String(contractshttp.StatusServiceUnavailable, err.Error()).Return(s.mockAbortableResponse).Once()
 
-	err := file.Create(path.Storage("framework/maintenance"), "")
-	require.NoError(t, err)
+	middleware := CheckForMaintenanceMode()
+	assert.PanicsWithError(s.T(), err.Error(), func() {
+		middleware(s.mockCtx)
+	})
+}
 
-	resp, err := client.Get(server.URL)
-	require.NoError(t, err)
-	assert.Equal(t, http.StatusServiceUnavailable, resp.StatusCode)
+func (s *MaintenanceTestSuite) TestMaintenaneMode_SecretDoesNotMatch() {
+	s.mockHash.EXPECT().Check("invalid-secret", "hashed-secret").Return(false).Once()
+	s.mockCtx.EXPECT().Request().Return(s.mockRequest).Once()
+	s.mockCtx.EXPECT().Response().Return(s.mockResponse).Once()
+	s.mockRequest.EXPECT().Query("secret", "").Return("invalid-secret").Once()
+	s.mockStorage.EXPECT().Exists("framework/maintenance.json").Return(true).Once()
+	s.mockStorage.EXPECT().GetBytes("framework/maintenance.json").Return([]byte(`{"secret":"hashed-secret", "reason": "Under Maintenance", "status": 503}`), nil).Once()
+	s.mockAbortableResponse.EXPECT().Abort().Return(nil).Once()
+	s.mockResponse.EXPECT().String(contractshttp.StatusServiceUnavailable, "Under Maintenance").Return(s.mockAbortableResponse).Once()
 
-	err = file.Remove(path.Storage("framework/maintenance"))
-	require.NoError(t, err)
+	middleware := CheckForMaintenanceMode()
+	middleware(s.mockCtx)
+}
 
-	resp, err = client.Get(server.URL)
-	require.NoError(t, err)
-	assert.Equal(t, http.StatusOK, resp.StatusCode)
+func (s *MaintenanceTestSuite) TestMaintenaneMode_SecretMatches() {
+	s.mockHash.EXPECT().Check("valid-secret", "hashed-secret").Return(true).Once()
+	s.mockCtx.EXPECT().Request().Return(s.mockRequest).Twice()
+	s.mockRequest.EXPECT().Next().Once()
+	s.mockRequest.EXPECT().Query("secret", "").Return("valid-secret").Once()
+	s.mockStorage.EXPECT().Exists("framework/maintenance.json").Return(true).Once()
+	s.mockStorage.EXPECT().GetBytes("framework/maintenance.json").Return([]byte(`{"secret":"hashed-secret", "reason": "Under Maintenance", "status": 503}`), nil).Once()
+
+	middleware := CheckForMaintenanceMode()
+	middleware(s.mockCtx)
+}
+
+func (s *MaintenanceTestSuite) TestMaintenaneMode_Redirect() {
+	s.mockCtx.EXPECT().Request().Return(s.mockRequest).Twice()
+	s.mockCtx.EXPECT().Response().Return(s.mockResponse).Once()
+	s.mockRequest.EXPECT().Query("secret", "").Return("").Once()
+	s.mockRequest.EXPECT().Path().Return("/").Once()
+	s.mockStorage.EXPECT().Exists("framework/maintenance.json").Return(true).Once()
+	s.mockStorage.EXPECT().GetBytes("framework/maintenance.json").Return([]byte(`{"redirect": "/maintenance", "status": 503}`), nil).Once()
+	s.mockResponse.EXPECT().Redirect(contractshttp.StatusTemporaryRedirect, "/maintenance").Return(s.mockAbortableResponse).Once()
+	s.mockAbortableResponse.EXPECT().Abort().Return(nil).Once()
+
+	middleware := CheckForMaintenanceMode()
+	middleware(s.mockCtx)
+}
+
+func (s *MaintenanceTestSuite) TestMaintenaneMode_Render() {
+	s.mockCtx.EXPECT().Request().Return(s.mockRequest).Twice()
+	s.mockRequest.EXPECT().Query("secret", "").Return("").Once()
+	s.mockStorage.EXPECT().Exists("framework/maintenance.json").Return(true).Once()
+	s.mockStorage.EXPECT().GetBytes("framework/maintenance.json").Return([]byte(`{"render": "maintenance", "status": 503}`), nil).Once()
+	s.mockRequest.EXPECT().Abort(contractshttp.StatusServiceUnavailable).Once()
+
+	mocksView := mockshttp.NewResponseView(s.T())
+	mocksHttpResponse := mockshttp.NewResponse(s.T())
+	mocksHttpResponse.EXPECT().Render().Return(nil).Once()
+	mocksView.EXPECT().Make("maintenance").Return(mocksHttpResponse).Once()
+	s.mockResponse.EXPECT().View().Return(mocksView).Once()
+	s.mockCtx.EXPECT().Response().Return(s.mockResponse).Once()
+
+	middleware := CheckForMaintenanceMode()
+	middleware(s.mockCtx)
 }
